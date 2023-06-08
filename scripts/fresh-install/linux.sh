e1b574c16a187f4ee7e42cb33d170ab37b70ffec
Alex Ruger <alexrugermusic@gmail.com>
2023-06-08 15:56:37 -0700

    huge update

--- scripts/fresh-install/linux.sh
+++ scripts/fresh-install/linux.sh
@@ -2,79 +2,28 @@
 
 
 # TODO:
-# remove snap
-# ssh key
-# download dotfiles, checkout linux-minimal
-# download this script
+# make ssh key, add to github account via `gh`
 
 
-function install_oh_my_zsh () {
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ╔════════════════════════════════════════════════════════════════════════════╗
+# ║ functions                                                                  ║
+# ╚════════════════════════════════════════════════════════════════════════════╝
+
+# not used for automation; simply here for easy copy/paste
+function download_this_script () {
+    wget --no-check-certificate --content-disposition 
+
 }
-
-
-function install_lazygit () {
-    if [[ if -d $HOME/src ]]; then cd $HOME/src; fi
-    git clone git@github.com:jesseduffield/lazygit.git
-    cd lazygit
-    go install
-}
-
-
-function install_tmux_from_source () {
-    # install dependencies
-    sudo apt install -y \
-        bison \
-        byacc \
-        libevent-dev \
-
-    if [[ if -d $HOME/src ]]; then cd $HOME/src; fi
-    git clone git@github.com:tmux/tmux.git
-    cd tmux
-    sh autogen.sh
-    ./configure && make
-    sudo make clean install
-}
-
-
-function install_phpenv_build_prerequisites () {
-    sudo apt install -y \
-        libcurl4-gnutls-dev \
-        libjpeg-dev \
-        libonig-dev \
-        libtidy-dev \
-        libzip-dev \
-}
-
-
-function install_firefox_without_snap () {
-    # assumes that snap has already been removed and `sudo snap remove firefox` run
-
-    # depends on software-properties-common package (installed in `install_from_package_manager()`
-    sudo add-apt-repository ppa:mozillateam/ppa
-
-    # alters the Firefox package priority to ensure the PPA/deb/apt version is preferred
-    echo '
-    Package: *
-    Pin: release o=LP-PPA-mozillateam
-    Pin-Priority: 1001
-    ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-
-    # ensures future Firefox upgrades are installed
-    echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-
-    sudo apt update && sudo apt install firefox
-}
-
 
 function install_from_package_manager () {
     # `uname --all` is bound to include some reference to the distro name
-    if [[ $(uname --all) == *"Ubuntu"* ]]; then
+    if [ $(uname --all) == *"Ubuntu"* ] || [ $(uname --all) == *"Debian"* ]; then
         sudo apt update && sudo apt upgrade -y
     
         sudo apt install \
             apache2 \
             bettercap \
+            btop \
             build-essential \
     		cmake \
             cmatrix \
@@ -83,11 +32,13 @@ function install_from_package_manager () {
     		docker \
             docker-compose \
             git \
+            golang-go \
     		hsetroot \
             htop \
     		libvirt-daemon \
     		llvm \
             lua \
+            lynx \
     		make \
     		neofetch \
     		ncdu \
@@ -109,47 +60,71 @@ function install_from_package_manager () {
 }
 
 
-function install_github_cli () {
-	type -p curl >/dev/null || sudo apt install curl -y
-	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+function remove_snap () {
+    if [ $(uname --all) == *"Ubuntu"* ]; then
+        # Returns list of snaps installed.
+        # This is a very simple usage of awk, so the first item is `Name`, i.e. the 
+        #   name of the first column. It will have to be skipped over when 
+        #   iterating over these later.
+        snap_list=$(snap list | awk '{print $1}')
+
+        # stop snap services
+        sudo systemctl disable snapd.service
+        sudo systemctl disable snapd.socket
+        sudo systemctl disable snapd.seeded.service
+
+        # In an effort to keep this POSIX compliant, arrays are avoided. Instead, 
+        #   utilizes `tr` to simulate.
+        # `tr ' ' '\n'` performs replaces spaces with new-lines, which are the 
+        #   default delimiter for `read`.
+        echo "$snap_list" | tr ' ' '\n' | while read item; do
+            # As mentioned above, the first item `Name` is skipped.
+            if "$item" != "Name"; then
+                sudo snap remove "$item"
+            fi
+        done
+
+        sudo rm -rf /var/cache/snapd
+
+        sudo apt autoremove --purge snapd
+
+        sudo rm -rf ~/snap
+    fi
 }
 
 
-function install_rust () {
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	cargo install \
-	    cargo-audi \
-	    cargo-edit \
-	    cargo-make \
-	    cargo-tarpaulin \
-	    cargo-watch
+function build_tmux_from_source () {
+    # for building new and updating
+    cd $HOME/src/tmux
+    git pull
+    sh autogen.sh
+    ./configure && make
+    sudo make clean install
 }
 
 
-function install_nvm () {
-    cd ~/src
-    git clone git@github.com:nvm-sh/nvm.git
-    ln -s ~/src/nvm/ ~/.nvm
+function install_tmux_from_source () {
+    # install dependencies
+    sudo apt install -y \
+        bison \
+        byacc \
+        libevent-dev
 
-    # from here, assumes that the following lines have been added to .bashrc, .zshrc, etc:
-    # export NVM_DIR="$HOME/.nvm"
-    # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/tmux/tmux.git
 
-    # should probably source .bashrc or .zshrc, and then...
-
-    nvm install node    # installs absolute latest version
-    nvm install --lts   # installs lts version
+    build_tmux_from_source
 }
 
 
+function install_tmux_package_manager () {
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/tmux-plugins/tpm.git
 
-function install_npm_apps () {
-	npm install gtop -g
+    if [ ! -d $HOME/.tmux/plugins ]; then mkdir -p $HOME/.tmux/plugins; fi
+	ln -s $HOME/src/tpm $HOME/.tmux/plugins/tpm
 }
 
 
@@ -169,10 +144,19 @@ function install_neovim_dependencies () {
 
 
 function build_neovim_from_source () {
-	cd ~/src/neovim
+    cd $HOME/src/neovim
 	git checkout stable
 	make CMAKE_BUILD_TYPE=RelWithDebInfo
 	sudo make install
+}
+
+
+function install_neovim_from_source () {
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/neovim/neovim.git
+
+    build_neovim_from_source
 }
 
 
@@ -183,20 +167,30 @@ function install_packer_nvim () {
 }
 
 
-function install_tmux_package_manager () {
-	cd ~/src
-	git clone git@github.com:tmux-plugins/tpm.git
-	ln -s ~/src/tpm ~/.tmux/plugins/tpm
+function install_nvm_from_source () {
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/nvm-sh/nvm.git
+    ln -s $HOME/src/nvm/ $HOME/.nvm
 }
 
 
-function install_fira_code_nerd_font () {
-    mkdir ~/.fonts
-    cd ~/.fonts
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip
-    unzip FiraCode.zip
-    rm FiraCode.zip
-    fc-cache -fv
+function install_phpenv_build_prerequisites () {
+    sudo apt install -y \
+        libcurl4-gnutls-dev \
+        libjpeg-dev \
+        libonig-dev \
+        libtidy-dev \
+        libzip-dev
+}
+
+
+function install_phpenv_from_source () {
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/phpenv/phpenv.git
+    cd $HOME/src/phpenv
+    ln -s $HOME/src/phpenv/ $HOME/.phpenv
 }
 
 
@@ -220,7 +214,363 @@ function install_pyenv_build_dependencies () {
 }
 
 
+function install_pyenv_from_source () {
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/pyenv/pyenv.git
+    cd $HOME/src/pyenv
+    ln -s $HOME/src/pyenv/ $HOME/.pyenv
+}
+
+
+function install_oh_my_zsh () {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    exec $SHELL
+}
+
+
+function prep_for_nvm_nodejs_installs () {
+    echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> $HOME/.zshrc
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.zshrc
+}
+
+
+function install_nodejs () {
+    # assumes that the following lines have been added to .bashrc, .zshrc, etc:
+
+    # export NVM_DIR="$HOME/.nvm"
+    # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+    # installs absolute latest version
+    nvm install node    
+
+    # installs lts version
+    nvm install --lts   
+    nvm use --lts
+}
+
+
+function prep_for_pyenv_python_installs () {
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.zshrc
+    command -v pyenv >/dev/null || echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.zshrc
+    echo 'eval "$(pyenv init -)"' >> $HOME/.zshrc
+}
+
+
+function install_python () {
+    # gets the system version (minus the word "Python"), then installs that 
+    #   version with pyenv and sets it to "global."
+    system_python_version=$(python3 --version | sed 's/^3')
+
+    # `s/.* //` deletes from the beginning to the first space
+    python_version=$(echo "$system_python_version" | sed 's/.* //')
+
+    pyenv install "$python_version"
+    pyenv global "$python_version"
+}
+
+
+function install_rust () {
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+
+function download_dotfiles () {
+    cd $HOME
+    git clone https://github.com/rewgs/dotfiles.git
+}
+
+
+# TODO: make sure this works on macOS!
+function make_dotfiles_symlinks () {
+    # ╔════════════════════════════════════════════════════════════════════════════╗
+    # ║ make directories                                                           ║
+    # ╚════════════════════════════════════════════════════════════════════════════╝
+    
+    if [[ ! -d $HOME/.config ]]; then
+        mkdir $HOME/.config
+    fi
+    
+    if [[ ! -d $HOME/dotfile_backups/bash ]]; then
+        mkdir -p $HOME/dotfile_backups/bash
+    fi
+    
+    if [[ ! -d $HOME/dotfile_backups/nvim ]]; then
+        mkdir -p $HOME/dotfile_backups/nvim
+    fi
+    
+    if [[ ! -d $HOME/dotfile_backups/sh ]]; then
+        mkdir -p $HOME/dotfile_backups/sh
+    fi
+    
+    if [[ ! -d $HOME/dotfile_backups/tmux ]]; then
+        mkdir -p $HOME/dotfile_backups/tmux
+    fi
+    
+    if [[ ! -d $HOME/dotfile_backups/zsh ]]; then
+        mkdir -p $HOME/dotfile_backups/zsh
+    fi
+    
+    # ╔════════════════════════════════════════════════════════════════════════════╗
+    # ║ create symlinks (and backup if target already present)                     ║
+    # ╚════════════════════════════════════════════════════════════════════════════╝
+    
+    # bash
+    if [[ -f $HOME/.bashrc ]]; then
+        mv $HOME/.bashrc $HOME/dotfile_backups/bashrc
+        ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
+    elif [[ -h $HOME/.bashrc ]]; then
+        rm $HOME/.bashrc
+        ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
+    else
+        ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
+    fi
+    
+    if [[ -f $HOME/.bash_profile ]]; then
+        mv $HOME/.bash_profile $HOME/dotfile_backups/bash_profile
+        ln -s $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
+    elif [[ -h $HOME/.bash_profile ]]; then
+        rm $HOME/.bash_profile
+        ln -s $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
+    else
+        ln -s $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
+    fi
+    
+
+    # nvim
+    if [[ -d $HOME/.config/nvim ]]; then
+        mv $HOME/.config/nvim $HOME/dotfile_backups/nvim
+        ln -s $HOME/dotfiles/nvim/ $HOME/.config/nvim
+    elif [[ -h $HOME/.config/nvim ]]; then
+        rm $HOME/.config/nvim/
+        ln -s $HOME/dotfiles/nvim/ $HOME/.config/nvim
+    else
+        ln -s $HOME/dotfiles/nvim/ $HOME/.config/nvim
+    fi
+    
+
+    # sh
+    if [[ -f $HOME/.profile ]]; then
+        mv $HOME/.profile $HOME/dotfile_backups/sh/
+        ln -s $HOME/dotfiles/sh/profile $HOME/.profile
+    elif [[ -h $HOME/.profile ]]; then
+        rm $HOME/.profile
+        ln -s $HOME/dotfiles/sh/profile $HOME/.profile
+    else
+        ln -s $HOME/dotfiles/sh/profile $HOME/.profile
+    fi
+    
+    # tmux
+    if [[ -f $HOME/.tmux.conf ]]; then
+        mv $HOME/.tmux.conf $HOME/dotfile_backups/tmux/
+        ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+    elif [[ -h $HOME/.tmux.conf ]]; then
+        rm $HOME/.tmux.conf
+        ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+    else
+        ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+    fi
+     
+    if [[ ! -d $HOME/src/tpm ]]; then
+        ln -s $HOME/src/tpm/ $HOME/.tmux/plugins/tpm
+    elif [[ -h $HOME/.tmux/plugins/tpm ]]; then
+        rm $HOME/.tmux/plugins/tpm
+        ln -s $HOME/src/tpm/ $HOME/.tmux/plugins/tpm
+    else
+        ln -s $HOME/src/tpm/ $HOME/.tmux/plugins/tpm
+    fi
+    
+    
+    # zsh
+    if [[ -f $HOME/.zshenv ]]; then
+        mv $HOME/.zshenv $HOME/dotfile_backups/zsh/
+        ln -s $HOME/dotfiles/zsh/zshenv $HOME/.zshenv
+    elif [[ -h $HOME/.zshenv ]]; then
+        rm $HOME/.zshenv
+        ln -s $HOME/dotfiles/zsh/zshenv $HOME/.zshenv
+    else
+        ln -s $HOME/dotfiles/zsh/zshenv $HOME/.zshenv
+    fi
+    
+    if [[ -f $HOME/.zprofile ]]; then
+        mv $HOME/.zprofile $HOME/dotfile_backups/zsh/
+        ln -s $HOME/dotfiles/zsh/zprofile $HOME/.zprofile
+    elif [[ -h $HOME/.zprofile ]]; then
+        rm $HOME/.zprofile
+        ln -s $HOME/dotfiles/zsh/zprofile $HOME/.zprofile
+    else
+        ln -s $HOME/dotfiles/zsh/zprofile $HOME/.zprofile
+    fi
+    
+    if [[ -f $HOME/.zshrc ]]; then
+        mv $HOME/.zshrc $HOME/dotfile_backups/zsh/
+        ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+    elif [[ -h $HOME/.zshrc ]]; then
+        rm $HOME/.zshrc
+        ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+    else
+        ln -s $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+    fi
+    
+    if [[ -f $HOME/.zlogin ]]; then
+        mv $HOME/.zlogin $HOME/dotfile_backups/zsh/
+        ln -s $HOME/dotfiles/zsh/zlogin $HOME/.zlogin
+    elif [[ -h $HOME/.zlogin ]]; then
+        rm $HOME/.zlogin
+        ln -s $HOME/dotfiles/zsh/zlogin $HOME/.zlogin
+    else
+        ln -s $HOME/dotfiles/zsh/zlogin $HOME/.zlogin
+    fi
+    
+    if [[ -f $HOME/.zlogout ]]; then
+        mv $HOME/.zlogout $HOME/dotfile_backups/zsh/
+        ln -s $HOME/dotfiles/zsh/zlogout $HOME/.zlogout
+    elif [[ -h $HOME/.zlogout ]]; then
+        rm $HOME/.zlogout
+        ln -s $HOME/dotfiles/zsh/zlogout $HOME/.zlogout
+    else
+        ln -s $HOME/dotfiles/zsh/zlogout $HOME/.zlogout
+    fi
+}
+
+
+function install_lazygit_from_source () {
+    if [ ! -d $HOME/src ]; then mkdir $HOME/src; fi
+    cd $HOME/src; 
+    git clone https://github.com/jesseduffield/lazygit.git
+    cd $HOME/src/lazygit
+    go install
+}
+
+
+function install_npm_apps () {
+	npm install gtop -g
+}
+
+
+function install_rust_apps () {
+	cargo install \
+	    cargo-audi \
+	    cargo-edit \
+	    cargo-make \
+	    cargo-tarpaulin \
+	    cargo-watch
+}
+
+
+function install_github_cli () {
+    # add repository
+	type -p curl >/dev/null || sudo apt install curl -y
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+
+	sudo apt update && sudo apt install gh -y
+}
+
+
+function install_firefox_without_snap () {
+    # assumes that snap has already been removed and `sudo snap remove firefox` run
+
+    # depends on software-properties-common package (installed in `install_from_package_manager()`
+    sudo add-apt-repository ppa:mozillateam/ppa
+
+    # alters the Firefox package priority to ensure the PPA/deb/apt version is preferred
+    echo '
+    Package: *
+    Pin: release o=LP-PPA-mozillateam
+    Pin-Priority: 1001
+    ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+    # ensures future Firefox upgrades are installed
+    echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+    sudo apt update && sudo apt install firefox
+}
+
+
+function install_fira_code_nerd_font () {
+    mkdir ~/.fonts
+    cd ~/.fonts
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip
+    unzip FiraCode.zip
+    rm FiraCode.zip
+    fc-cache -fv
+}
+
+
+# ╔════════════════════════════════════════════════════════════════════════════╗
+# ║ macros                                                                     ║
+# ║                                                                            ║
+# ║ These are comprised of the individual functions above.                     ║
+# ╚════════════════════════════════════════════════════════════════════════════╝
+
+# for servers or any other machine I'm not coding on but want to feel "at home"
+function basic_setup () {
+    # basics
+    install_from_package_manager
+    remove_snap
+
+    # tmux
+    install_tmux_from_source
+    install_tmux_package_manager
+
+    # neovim
+    install_neovim_dependencies
+    install_neovim_from_source
+    install_packer_nvim
+
+    # nodejs
+    install_nvm_from_source
+
+    # phpenv
+    install_phpenv_build_prerequisites
+    install_phpenv_from_source
+
+    # pyenv
+    install_pyenv_build_dependencies
+    install_pyenv_from_source
+
+    install_oh_my_zsh
+
+    prep_for_nvm_nodejs_installs
+    install_nodejs
+
+    prep_for_pyenv_python_installs
+    install_python
+
+    install_rust
+}
+
+
+function personal_setup_cli () {
+    download_dotfiles
+    make_dotfiles_symlinks
+
+    install_lazygit_from_source
+    install_npm_apps
+    install_rust_apps
+    install_github_cli
+}
+
+
+function personal_setup_gui () {
+    install_firefox_without_snap
+    install_fira_code_nerd_font
+}
+
+# ╔════════════════════════════════════════════════════════════════════════════╗
+# ║ main                                                                       ║
+# ║                                                                            ║
+# ║ Comprised of one or more macros; separated by machine usage.               ║
+# ║ These don't overlap at all -- instead they cascade/build upon each other.  ║
+# ╚════════════════════════════════════════════════════════════════════════════╝
+
 function main () {
+    basic_setup
+    personal_setup_cli
+    # personal_setup_gui
 }
 
 main
