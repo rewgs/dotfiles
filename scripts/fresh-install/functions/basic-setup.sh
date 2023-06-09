2542cbafce7c34b4a753d758a71f70c4e71240e7
Alex Ruger <alexrugermusic@gmail.com>
2023-06-09 16:03:11 -0700

    pyenv fix

--- scripts/fresh-install/functions/basic-setup.sh
+++ scripts/fresh-install/functions/basic-setup.sh
@@ -300,6 +300,10 @@ install_nodejs() {
 
 
 prep_for_pyenv_python_installs() {
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+
     echo 'export PYENV_ROOT=""$HOME"/.pyenv"' >> "$HOME"/.zshrc
     echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> "$HOME"/.zshrc
     echo 'eval "$(pyenv init -)"' >> "$HOME"/.zshrc
