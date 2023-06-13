f49999a4adec6e59c5eed087942ec03b7ebb8da0
Alex Ruger <alexrugermusic@gmail.com>
2023-06-13 18:06:55 -0500

    updated phpenv

--- scripts/fresh-install/functions/basic-setup.sh
+++ scripts/fresh-install/functions/basic-setup.sh
@@ -231,11 +231,13 @@ install_nvm_from_source() {
 
 install_phpenv_build_prerequisites() {
     sudo apt install -y \
+        apache2-dev \
         libcurl4-gnutls-dev \
         libjpeg-dev \
         libonig-dev \
         libtidy-dev \
-        libzip-dev
+        libzip-dev \
+        re2c \
 }
 
 
@@ -251,6 +253,10 @@ install_phpenv_from_source() {
     git clone --depth 1 https://github.com/php-build/php-build.git
     if [ ! -d ~/.phpenv/plugins ]; then mkdir -p ~/.phpenv/plugins; fi
     ln -s ~/src/php-build/ ~/.phpenv/plugins/php-build
+
+    # required for phpenv to see php-build
+    # should also be run after each new version of php is installed
+    phpenv rehash
 }
 
 
@@ -375,8 +381,9 @@ main() {
     install_nodejs
 
     # phpenv
-    install_phpenv_build_prerequisites
-    install_phpenv_from_source
+    # NOTE: php-build is broken at the moment so this isn't worth installing
+    # install_phpenv_build_prerequisites
+    # install_phpenv_from_source
 
     # pyenv
     install_pyenv_build_dependencies
