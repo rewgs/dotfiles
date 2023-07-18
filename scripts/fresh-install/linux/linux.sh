6c90fe6bfb004822730c9786ebf9b28bcd2287ac
Alex Ruger <alexrugermusic@gmail.com>
2023-07-17 18:18:36 -0700

    changed path

--- scripts/fresh-install/linux/linux.sh
+++ scripts/fresh-install/linux/linux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-cd ~/dotfiles/scripts/fresh-install
+cd ~/dotfiles/scripts/fresh-install/linux
 . ./functions/basic-setup.sh
 
-# cd ~/dotfiles/scripts/fresh-install
+# cd ~/dotfiles/scripts/fresh-install/linux
 # . ./functions/personal-setup.sh
