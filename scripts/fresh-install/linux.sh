789b3ff24db3a91949eaea17ce7486c1777d6c8d
Alex Ruger <alexrugermusic@gmail.com>
2023-07-23 21:14:34 -0700

    began redoing dotfile symlinking function

--- scripts/fresh-install/linux.sh
+++ scripts/fresh-install/linux.sh
@@ -304,7 +304,7 @@ function install_nodejs() {
 
     # installs lts version
     nvm install --lts   
-    # nvm use --lts
+    nvm use --lts
 }
 
 
@@ -420,94 +420,40 @@ function install_cargo_apps () {
 
 
 function make_dotfiles_symlinks () {
-    # ╔═════════════════════════════════════════════════════════╗
-    # ║ make directories                                        ║
-    # ╚═════════════════════════════════════════════════════════╝
-    
-    if [ ! -d "$HOME"/.config ]; then
-        mkdir "$HOME"/.config
-    fi
-    
-    if [ ! -d "$HOME"/dotfile_backups/bash ]; then
-        mkdir -p "$HOME"/dotfile_backups/bash
-    fi
-    
-    if [ ! -d "$HOME"/dotfile_backups/nvim ]; then
-        mkdir -p "$HOME"/dotfile_backups/nvim
-    fi
-    
-    if [ ! -d "$HOME"/dotfile_backups/sh ]; then
-        mkdir -p "$HOME"/dotfile_backups/sh
-    fi
-    
-    if [ ! -d "$HOME"/dotfile_backups/tmux ]; then
-        mkdir -p "$HOME"/dotfile_backups/tmux
-    fi
-    
-    if [ ! -d "$HOME"/dotfile_backups/zsh ]; then
-        mkdir -p "$HOME"/dotfile_backups/zsh
-    fi
-    
-    # ╔═════════════════════════════════════════════════════════╗
-    # ║ create symlinks (and backup if target already present)  ║
-    # ╚═════════════════════════════════════════════════════════╝
-    
     # bash
-    if [ -f "$HOME"/.bashrc ]; then
-        mv "$HOME"/.bashrc "$HOME"/dotfile_backups/bashrc
-        ln -s "$HOME"/dotfiles/bash/bashrc "$HOME"/.bashrc
-    elif [ -h "$HOME"/.bashrc ]; then
-        rm "$HOME"/.bashrc
-        ln -s "$HOME"/dotfiles/bash/bashrc "$HOME"/.bashrc
-    else
-        ln -s "$HOME"/dotfiles/bash/bashrc "$HOME"/.bashrc
-    fi
-    
-    if [ -f "$HOME"/.bash_profile ]; then
-        mv "$HOME"/.bash_profile "$HOME"/dotfile_backups/bash_profile
-        ln -s "$HOME"/dotfiles/bash/bash_profile "$HOME"/.bash_profile
-    elif [ -h "$HOME"/.bash_profile ]; then
-        rm "$HOME"/.bash_profile
-        ln -s "$HOME"/dotfiles/bash/bash_profile "$HOME"/.bash_profile
-    else
-        ln -s "$HOME"/dotfiles/bash/bash_profile "$HOME"/.bash_profile
-    fi
-    
+    typeset -a bash_dots
+    bash_dots=(
+        "bashrc"
+        "bash_profile"
+    )
+    for d in "${bash_dots[@]}"; do
+        if [ -f "$HOME/.$d" ] || [ -h "$HOME/.$d" ]; then
+            rm "$HOME/.$d"
+        fi
+        ln -s "$HOME/dotfiles/bash/$d" "$HOME/.$d"
+    done
 
     # nvim
-    if [ -d "$HOME"/.config/nvim ]; then
-        mv "$HOME"/.config/nvim "$HOME"/dotfile_backups/nvim
-        ln -s "$HOME"/dotfiles/nvim/ "$HOME"/.config/nvim
-    elif [ -h "$HOME"/.config/nvim ]; then
-        rm "$HOME"/.config/nvim/
-        ln -s "$HOME"/dotfiles/nvim/ "$HOME"/.config/nvim
-    else
-        ln -s "$HOME"/dotfiles/nvim/ "$HOME"/.config/nvim
+    if [ ! -d "$HOME/.config" ]; then
+        mkdir "$HOME/.config"
     fi
-    
+    if [ -d "$HOME/.config/nvim" ] || [ -h "$HOME/.config/nvim" ]; then
+        ln -s "$HOME/dotfiles/nvim/" "$HOME/.config/nvim"
+    fi
 
     # sh
-    if [ -f "$HOME"/.profile ]; then
-        mv "$HOME"/.profile "$HOME"/dotfile_backups/sh/
-        ln -s "$HOME"/dotfiles/sh/profile "$HOME"/.profile
-    elif [ -h "$HOME"/.profile ]; then
-        rm "$HOME"/.profile
-        ln -s "$HOME"/dotfiles/sh/profile "$HOME"/.profile
-    else
-        ln -s "$HOME"/dotfiles/sh/profile "$HOME"/.profile
+    if [ -f "$HOME/.profile" ] || [ -h "$HOME/.profile" ]; then
+        rm "$HOME/.profile"
     fi
-    
+    ln -s "$HOME/dotfiles/sh/profile" "$HOME/.profile"
+
     # tmux
-    if [ -f "$HOME"/.tmux.conf ]; then
-        mv "$HOME"/.tmux.conf "$HOME"/dotfile_backups/tmux/
-        ln -s "$HOME"/dotfiles/tmux/tmux.conf "$HOME"/.tmux.conf
-    elif [ -h "$HOME"/.tmux.conf ]; then
-        rm "$HOME"/.tmux.conf
-        ln -s "$HOME"/dotfiles/tmux/tmux.conf "$HOME"/.tmux.conf
-    else
-        ln -s "$HOME"/dotfiles/tmux/tmux.conf "$HOME"/.tmux.conf
+    if [ -f "$HOME/.tmux.conf" ] || [ -h "$HOME/.tmux.conf" ]; then
+        rm "$HOME/.tmux.conf"
     fi
+    ln -s "$HOME/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
      
+    # tpm - REDO THIS NEXT
     if [ ! -d "$HOME"/src/tpm ]; then
         ln -s "$HOME"/src/tpm/ "$HOME"/.tmux/plugins/tpm
     elif [ -h "$HOME"/.tmux/plugins/tpm ]; then
