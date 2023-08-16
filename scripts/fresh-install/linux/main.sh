cca776b2079dc4d59257cd0d1fd75a1941a12ad3
Alex Ruger <alexrugermusic@gmail.com>
2023-08-16 11:41:22 -0700

    added tailscale installation

--- scripts/fresh-install/linux/main.sh
+++ scripts/fresh-install/linux/main.sh
@@ -489,6 +489,19 @@ function install_firefox_without_snap () {
 }
 
 
+function install_tailscale () {
+    # Add Tailscale’s package signing key and repository:
+    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+    sudo apt-get update
+    sudo apt-get install tailscale
+
+    sudo tailscale up
+    sudo tailscale up --ssh
+}
+
+
 function main () {
     function part1 () {
         # This isn't really working. Commenting out for now.
