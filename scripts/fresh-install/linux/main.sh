5789bd48946821f86e91e2a23419c55ec1e607a7
Alex Ruger <alexrugermusic@gmail.com>
2023-09-09 23:49:33 -0700

    added logging

--- scripts/fresh-install/linux/main.sh
+++ scripts/fresh-install/linux/main.sh
@@ -144,19 +144,32 @@ function make_dotfiles_symlinks () {
 
 
 function main () {
+    current=$(pwd)
+    log_file="$current/installation_log.txt"
+
+    cd "$current" || return
     source ./utils.sh
     # This isn't really working. Commenting out for now.
     # remove_snap
     
+    cd "$current" || return
+    echo "Running install_apps_from_package_manager()" | cat >> "$log_file"
     install_apps_from_package_manager
+    echo "install_apps_from_package_manager() finished!" | cat >> "$log_file"
 
+    cd "$current" || return
     source ./misc_installs.sh
+    echo "Running install_github_cli()" | cat >> "$log_file"
     install_github_cli
+    echo "install_github_cli() finished!" | cat >> "$log_file"
     
     # tmux
+    cd "$current" || return
     source ./tmux.sh
+    echo "Installing tmux and tpm" | cat >> "$log_file"
     install_tmux_from_source
     install_tmux_package_manager
+    echo "tmux and tpm installation finished!" | cat >> "$log_file"
 
     # for some reason, the following isn't running:
     # - neovim
@@ -166,32 +179,52 @@ function main () {
     # ...but then install_oh_my_zsh runs. wtf?
     
     # neovim
+    cd "$current" || return
     source ./neovim.sh
+    echo "Installing neovim" | cat >> "$log_file"
     install_neovim_dependencies
     build_neovim_from_source
     install_packer_nvim
+    echo "neovim installation finished!" | cat >> "$log_file"
     
     # nodejs
+    cd "$current" || return
     source ./nodejs.sh
+    echo "Installing nvm and nodejs" | cat >> "$log_file"
     install_nvm_from_source
     prep_for_nvm_nodejs_installs
     install_nodejs
+    echo "nvm and nodejs installation finished!" | cat >> "$log_file"
     
     # phpenv
+    cd "$current" || return
     source ./phpenv.sh
+    echo "Installing phpenv" | cat >> "$log_file"
     install_phpenv_build_prerequisites
     install_phpenv_from_source
+    echo "phpenv installation finished!" | cat >> "$log_file"
     
     # pyenv
+    cd "$current" || return
     source ./pyenv.sh
+    echo "Installing pyenv" | cat >> "$log_file"
     install_pyenv_build_dependencies
     install_pyenv_from_source
+    echo "pyenv installation finished!" | cat >> "$log_file"
+    echo "Installing Python" | cat >> "$log_file"
     prep_for_pyenv_python_installs
     install_python
+    echo "Python installation finished!" | cat >> "$log_file"
     
+    echo "Installing ohmyzsh..." | cat >> "$log_file"
     install_oh_my_zsh
+    echo "ohmyzsh installation finished!" | cat >> "$log_file"
+    echo "Changing shell to zsh..." | cat >> "$log_file"
     sudo chsh -s "$(which zsh)" "$(whoami)"
+    echo "Shell should now be zsh!" | cat >> "$log_file"
+    echo "Symlinking to dotfiles..." | cat >> "$log_file"
     make_dotfiles_symlinks
+    echo "Dotfiles symlinked!" | cat >> "$log_file"
     
     echo "All done! Restarting now..."
     sudo reboot
