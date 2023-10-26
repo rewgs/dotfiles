d05d34d0b1debdd6ca44574a4c2ea9eb3368f0b1
Alex Ruger <alexrugermusic@gmail.com>
2023-10-25 17:03:44 -0700

    split everything out of main.sh except for main()

--- scripts/fresh-install/linux/utils.sh
+++ scripts/fresh-install/linux/utils.sh
@@ -1,3 +1,73 @@
+function make_dotfiles_symlinks () {
+    # bash
+    typeset -a bash_dots
+    bash_dots=(
+        "bashrc"
+        "bash_profile"
+    )
+    for d in "${bash_dots[@]}"; do
+        if [ -f "$HOME/.$d" ] || [ -h "$HOME/.$d" ]; then
+            rm "$HOME/.$d"
+        fi
+        ln -s "$HOME/dotfiles/bash/$d" "$HOME/.$d"
+    done
+
+    # nvim
+    if [ ! -d "$HOME/.config" ]; then
+        mkdir "$HOME/.config"
+    fi
+    if [ -d "$HOME/.config/nvim" ] || [ -h "$HOME/.config/nvim" ]; then
+        rm "$HOME/.config/nvim"
+    fi
+    ln -s "$HOME/dotfiles/nvim/" "$HOME/.config/nvim"
+
+    # sh
+    if [ -f "$HOME/.profile" ] || [ -h "$HOME/.profile" ]; then
+        rm "$HOME/.profile"
+    fi
+    ln -s "$HOME/dotfiles/sh/profile" "$HOME/.profile"
+
+    # tmux
+    if [ -f "$HOME/.tmux.conf" ] || [ -h "$HOME/.tmux.conf" ]; then
+        rm "$HOME/.tmux.conf"
+    fi
+    ln -s "$HOME/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
+     
+    # tpm
+    if [ -h "$HOME"/.tmux/plugins/tpm ]; then
+        rm "$HOME"/.tmux/plugins/tpm
+    fi
+    ln -s "$HOME/src/tpm/" "$HOME/.tmux/plugins/tpm"
+    
+    # zsh
+    typeset -a zsh_dots
+    zsh_dots=(
+        "zlogin"
+        "zlogout"
+        "zprofile"
+        "zshenv"
+        "zshrc"
+    )
+    for z in "${zsh_dots[@]}"; do
+        if [ -f "$HOME/.$z" ] || [ -h "$HOME/.$z" ]; then
+            rm "$HOME/.$z"
+        fi
+        ln -s "$HOME/dotfiles/zsh/$z" "$HOME/.$z"
+    done
+
+    echo "Symlinking dotfiles complete!"
+}
+
+
+function prevent_prompts () {
+    # Prevents prompts for restarting services
+    sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+
+    # Prevents prompts for restarting due to kernel updates
+    sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
+}
+
+
 function get_distro () {
     ( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1
 }
