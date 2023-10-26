979caaa922f683a078610f943b2055a02eea836a
Alex Ruger <alexrugermusic@gmail.com>
2023-10-25 20:26:27 -0700

    made all function calls subshells run as sudo...maybe this will work better

--- scripts/fresh-install/linux/main.sh
+++ scripts/fresh-install/linux/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-function main () {
+function step_1 () {
     current=$(pwd)
     log_file="$current/installation_log.txt"
 
@@ -17,21 +17,21 @@ function main () {
     
     cd "$current" || return
     echo "Running install_apps_from_package_manager()" | cat >> "$log_file"
-    install_apps_from_package_manager
+    sudo bash -c install_apps_from_package_manager
     echo "install_apps_from_package_manager() finished!" | cat >> "$log_file"
 
     cd "$current" || return
     source ./misc_installs.sh
     echo "Running install_github_cli()" | cat >> "$log_file"
-    install_github_cli
+    sudo bash -c install_github_cli
     echo "install_github_cli() finished!" | cat >> "$log_file"
     
     # tmux
     cd "$current" || return
     source ./tmux.sh
     echo "Installing tmux and tpm" | cat >> "$log_file"
-    install_tmux_from_source
-    install_tmux_package_manager
+    sudo bash -c install_tmux_from_source
+    sudo bash -c install_tmux_package_manager
     echo "tmux and tpm installation finished!" | cat >> "$log_file"
 
     # for some reason, the following isn't running:
@@ -45,64 +45,73 @@ function main () {
     cd "$current" || return
     source ./neovim.sh
     echo "Installing neovim" | cat >> "$log_file"
-    install_neovim_dependencies
-    build_neovim_from_source
-    install_packer_nvim
+    sudo bash -c install_neovim_dependencies
+    sudo bash -c build_neovim_from_source
+    sudo bash -c install_packer_nvim
     echo "neovim installation finished!" | cat >> "$log_file"
     
     # nodejs
     cd "$current" || return
     source ./nodejs.sh
     echo "Installing nvm and nodejs" | cat >> "$log_file"
-    install_nvm_from_source
-    prep_for_nvm_nodejs_installs
-    install_nodejs
+    sudo bash -c install_nvm_from_source
+    sudo bash -c prep_for_nvm_nodejs_installs
+    sudo bash -c install_nodejs
     echo "nvm and nodejs installation finished!" | cat >> "$log_file"
     
     # phpenv
     cd "$current" || return
     source ./phpenv.sh
     echo "Installing phpenv" | cat >> "$log_file"
-    install_phpenv_build_prerequisites
-    install_phpenv_from_source
+    sudo bash -c install_phpenv_build_prerequisites
+    sudo bash -c install_phpenv_from_source
     echo "phpenv installation finished!" | cat >> "$log_file"
     
     # pyenv
     cd "$current" || return
     source ./pyenv.sh
     echo "Installing pyenv" | cat >> "$log_file"
-    install_pyenv_build_dependencies
-    install_pyenv_from_source
+    sudo bash -c install_pyenv_build_dependencies
+    sudo bash -c install_pyenv_from_source
     echo "pyenv installation finished!" | cat >> "$log_file"
     # echo "Installing Python" | cat >> "$log_file"
-    prep_for_pyenv_python_installs
+    sudo bash -c prep_for_pyenv_python_installs
     # python isn't being installed for some reason
-    # install_python
+    sudo bash -c install_python
     # echo "Python installation finished!" | cat >> "$log_file"
     
     echo "Installing ohmyzsh..." | cat >> "$log_file"
-    install_oh_my_zsh
+    sudo bash -c install_oh_my_zsh
     echo "ohmyzsh installation finished!" | cat >> "$log_file"
     echo "Changing shell to zsh..." | cat >> "$log_file"
-    chsh -s "$(which zsh)" "$(whoami)"
+    sudo chsh -s "$(which zsh)" "$(whoami)"
     echo "Shell should now be zsh!" | cat >> "$log_file"
     echo "Symlinking to dotfiles..." | cat >> "$log_file"
-    make_dotfiles_symlinks
+    bash -c make_dotfiles_symlinks
     echo "Dotfiles symlinked!" | cat >> "$log_file"
     
     echo "All done! Restarting now..."
     reboot
     
+}
+
+function step_2 () {
+    current=$(pwd)
+    log_file="$current/installation_log.txt"
+
     cd "$current" || return
     source ./rust.sh
-    install_rust
-    install_cargo_apps
+    sudo zsh -c install_rust
+    sudo zsh -c install_cargo_apps
 
     # not working for some reason
-    # install_npm_apps
+    source ./nodejs.sh
+    sudo zsh -c install_npm_apps
 
     cd "$current" || return
     source ./gui.sh
-    install_fira_code_nerd_font
+    sudo zsh -c install_fira_code_nerd_font
 }
-main
+
+sudo bash -c step_1
+sudo bash -c step_2
