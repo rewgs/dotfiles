c770874b50f7744da93fb1fe022f50db071c8428
Alex Ruger <alexrugermusic@gmail.com>
2023-11-19 17:10:21 -0800

    cleanup

--- scripts/fresh-install/linux/main.sh
+++ scripts/fresh-install/linux/main.sh
@@ -1,78 +1,72 @@
 #!/usr/bin/env bash
 
-function step_1 {
-    this_repo=$(pwd)
-    # log_file="$this_repo/installation_log.txt"
-    
-    cd "$this_repo" || return
+function basic_environment_setup {
+    cd "$1" || return
     source ./distros.sh
-    distro=$(get_distro)
+    # distro=$(get_distro)
 
-    cd "$this_repo" || return
+    cd "$1" || return
     source ./packages/packages.sh
-    install_packages "$distro"
+    install_packages "$(get_distro)"
 
-    cd "$this_repo" || return
+    cd "$1" || return
     source ./packages/misc.sh
     install_oh_my_zsh
     chsh -s "$(which zsh)" "$(whoami)"
 
-    cd "$this_repo" || return
+    cd "$1" || return
     source ./symlink_dotfiles.sh
     make_zsh_symlinks
     
     echo "All done! Restarting now..."
     reboot
-
-    # tmux
-    # cd "$this_repo" || return
-    # source ./tmux.sh
-    # install_tmux_from_source
-    # install_tmux_package_manager
-
-    # neovim
-    # cd "$this_repo" || return
-    # source ./neovim.sh
-    # install_neovim_dependencies
-    # build_neovim_from_source
-    # echo "Success: neovim installation" | cat >> "$log_file"
-    # install_packer_nvim
-    # echo "Success: packer.nvim installation" | cat >> "$log_file"
-    
-    # nodejs
-    # cd "$this_repo" || return
-    # source ./nodejs.sh
-    # install_nvm_from_source
-    # echo "Success: nvm installation" | cat >> "$log_file"
-    # prep_for_nvm_nodejs_installs
-    # install_nodejs
-    # echo "Success: nodejs installation" | cat >> "$log_file"
-    
-    # phpenv
-    # cd "$this_repo" || return
-    # source ./phpenv.sh
-    # install_phpenv_build_prerequisites
-    # install_phpenv_from_source
-    # echo "Success: phpenv installation" | cat >> "$log_file"
-    
-    # pyenv
-    # cd "$this_repo" || return
-    # source ./pyenv.sh
-    # install_pyenv_build_dependencies
-    # install_pyenv_from_source
-    # echo "Success: pyenv installation" | cat >> "$log_file"
-    # prep_for_pyenv_python_installs
-    # install_python
-    # echo "Success: python installation" | cat >> "$log_file"
-    
 }
 
+function main_tools_setup {
+    # tmux
+    cd "$1" || return
+    source ./tmux.sh
+    install_tmux_from_source
+    install_tmux_package_manager
 
-function step_2 {
-    this_repo=$(pwd)
-    log_file="$this_repo/installation_log.txt"
+    # neovim
+    cd "$1" || return
+    source ./neovim.sh
+    install_neovim_dependencies
+    build_neovim_from_source
+    echo "Success: neovim installation" | cat >> "$log_file"
+    install_packer_nvim
+    echo "Success: packer.nvim installation" | cat >> "$log_file"
+}
 
-    cd "$this_repo" || return
+function install_languages {
+    # nodejs
+    cd "$1" || return
+    source ./nodejs.sh
+    install_nvm_from_source
+    echo "Success: nvm installation" | cat >> "$log_file"
+    prep_for_nvm_nodejs_installs
+    install_nodejs
+    echo "Success: nodejs installation" | cat >> "$log_file"
+    
+    # phpenv
+    cd "$1" || return
+    source ./phpenv.sh
+    install_phpenv_build_prerequisites
+    install_phpenv_from_source
+    echo "Success: phpenv installation" | cat >> "$log_file"
+    
+    # pyenv
+    cd "$1" || return
+    source ./pyenv.sh
+    install_pyenv_build_dependencies
+    install_pyenv_from_source
+    echo "Success: pyenv installation" | cat >> "$log_file"
+    prep_for_pyenv_python_installs
+    install_python
+    echo "Success: python installation" | cat >> "$log_file"
+
+    cd "$1" || return
     source ./rust.sh
     zsh -c install_rust
     zsh -c install_cargo_apps
@@ -80,12 +74,21 @@ function step_2 {
     # not working for some reason
     source ./nodejs.sh
     zsh -c install_npm_apps
+}
 
-    cd "$this_repo" || return
+function setup_gui {
+    cd "$1" || return
     source ./gui.sh
     zsh -c install_fira_code_nerd_font
 }
 
 
-step_1
-# step_2
+function main {
+    this_repo=$(pwd)
+    # log_file="$this_repo/installation_log.txt"
+
+    basic_environment_setup "$this_repo"
+    # main_tools_setup "$this_repo"
+    # install_languages "$this_repo"
+    # setup_gui "$this_repo"
+}
