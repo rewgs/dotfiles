d7e974c2148713cdf9dac2376aad2c9f67807e0b
Alex Ruger <alexrugermusic@gmail.com>
2023-11-24 14:38:33 -0800

    removed .DS_Store

--- hammerspoon/apps.lua
+++ hammerspoon/apps.lua
@@ -4,13 +4,12 @@ TERMINAL = 'iTerm'
 BROWSER = 'Google Chrome'
 
 local app_maps = {
-    { key = 'b', app = BROWSER },
-    { key = 'c', app = 'Visual Studio Code' },
-    { key = 'd', app = 'Discord' },      -- would like to key-chord this so that mod.hyper+m,{something} calls different messaging apps, e.g. mod.hyper+m,d calls Discord
-    { key = 'f', app = 'Finder' },
+    { key = 'b',      app = BROWSER },
+    { key = 'c',      app = 'Visual Studio Code' },
+    { key = 'd',      app = 'Discord' }, -- would like to key-chord this so that mod.hyper+m,{something} calls different messaging apps, e.g. mod.hyper+m,d calls Discord
+    { key = 'f',      app = 'Finder' },
     { key = 'q',      app = 'Cubase 12' },
-    -- { key = 'r',      app = 'REAPER' }, -- conflicts with reload
-    { key = 't', app = TERMINAL },
+    { key = 't',      app = TERMINAL },
     { key = 'return', app = TERMINAL }
 }
 
