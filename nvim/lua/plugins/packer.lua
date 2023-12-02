f2bcc0756ce43928a60460994f0208def0b979ae
Alex Ruger <alexrugermusic@gmail.com>
2023-12-02 13:17:36 -0800

    safety push

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -69,9 +69,9 @@ return require('packer').startup(function(use)
     use { "folke/trouble.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
         },
     }
 end)
