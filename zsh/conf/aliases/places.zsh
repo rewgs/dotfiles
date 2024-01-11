6223ebd8009c0fa484ed477ec7b74f2c2aa34fef
Alex Ruger <alexrugermusic@gmail.com>
2024-01-11 14:07:04 -0800

    changed dotfiles/etc aliases to point to /Users/rewgs/dotfiles; put aliases sourcing at bottom of zshrc so that /Users/rewgs/dotfiles env var is seen

--- zsh/conf/aliases/places.zsh
+++ zsh/conf/aliases/places.zsh
@@ -3,10 +3,9 @@ alias macmini="ssh rewgs@rewgs-mac-mini.local"
 alias nas="ssh rewgs@rewgs-nas"
 
 # dotfiles
-alias dotfiles="cd $HOME/dotfiles/"
-alias dots="cd $HOME/dotfiles/"
-alias d="cd $HOME/dotfiles/"
-alias dn="cd $HOME/dotfiles/nvim/"
+alias dotfiles="cd $DOTFILES"
+alias dots="cd $DOTFILES"
+alias d="cd $DOTFILES"
 
 # sns dirs
 alias work="cd $HOME/work/"
