559ddeb6532ab0cc2ae9c50daf2755ed75823522
Alex Ruger <alexrugermusic@gmail.com>
2024-02-10 19:53:02 -0800

    removed reaper.nivm...never use

--- nvim/lua/packer/config/reaper-nvim.lua
+++
@@ -1,6 +0,0 @@
-require('reaper-nvim').setup()
-
-vim.g.reaper_fuzzy_command = "fzf"
--- vim.g.reaper_target_port = 1234
-vim.g.reaper_target_ip = "127.0.0.1"
--- vim.g.reaper_browser_command = "firefox"
