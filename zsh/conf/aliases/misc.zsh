5f2f01fe4e952189774cbea8bf4e90f9b9eff5a3
Alex Ruger <alexrugermusic@gmail.com>
2024-03-19 17:39:34 -0700

    don't remember

--- zsh/conf/aliases/misc.zsh
+++ zsh/conf/aliases/misc.zsh
@@ -1,9 +1,12 @@
 # TODO: fix this, not currently working
 alias b="br" # invokes broot in power-user mode (br)
 
+# editors that aren't (n)vim
 alias h="hx"    # helix
 alias k="kak"   # kakoune
 alias n="nano"  # nano
+
+alias f="fzf"
 
 # sed
 if [[ $(uname) == "Darwin" ]]; then
