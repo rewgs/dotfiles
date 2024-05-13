1e04e7f3542d4ad1fa52831ac9b99220ca09b025
Alex Ruger <alexrugermusic@gmail.com>
2024-05-13 17:00:25 -0400

    minor edits

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -6,13 +6,19 @@ $profileFile = "Microsoft.PowerShell_profile.ps1"
 $src = Join-Path -Path $thisDir -ChildPath $profileFile
 
 if ($IsWindows) {
-    $dst = "~/Documents/PowerShell/$profileFile"
+    $dstDir = "~/Documents/PowerShell"
 } 
 else {
     $dstDir = "~/.config/powershell"
-    if (-Not (Test-Path $dstDir)) {
-        New-Item -Type Directory $dstDir
-    }
-    $dst = Join-Path -Path $dstDir -ChildPath $profileFile
+}
+
+# Creates $dstDir if it or a part of it doesn't exist (a la `mkdir -p`)
+if (-Not (Test-Path $dstDir)) {
+    New-Item -Type Directory $dstDir
+}
+
+$dst = Join-Path -Path $dstDir -ChildPath $profileFile
+if (Test-Path $dst) {
+    Remove-Item -Path $dst
 }
 New-Item -Path $dst -ItemType SymbolicLink -Value $src
