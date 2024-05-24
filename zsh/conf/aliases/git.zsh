7ca21907b7cdc57a3346d4e8283ac00b295dc3c1
rewgs <alexrugermusic@gmail.com>
2024-05-24 13:19:28 -0700

    added git branch remove function and aliases

--- zsh/conf/aliases/git.zsh
+++ zsh/conf/aliases/git.zsh
@@ -3,6 +3,9 @@ alias gap=git_add_all_and_push_with_commit_message_arg
 alias gp="git pull"
 alias gfarp="git fetch --all && git reset --hard && git pull"
 alias grm=git_commit_readme
+alias gdb="git_delete_branch $1 $2"
+alias grb="git branch -r" # lists all remote branches
+alias grbv="git branch -r -v" # lists all remote branches with the latest commit hash and commit message
 
 # git clients
 alias g="git-graph"
