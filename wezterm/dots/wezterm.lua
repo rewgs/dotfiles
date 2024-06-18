c96536a37b4047c5129626f5372a0a05f7b6706e
Alex Ruger <alexrugermusic@gmail.com>
2024-06-17 20:07:45 -0700

    minor tweak

--- wezterm/dots/wezterm.lua
+++ wezterm/dots/wezterm.lua
@@ -6,17 +6,15 @@ local config = wezterm.config_builder()
 
 local _ = require('common.helpers') -- requires only the helper functions; this is placed in common for cleanliness
 local common_config = require('common.init') -- requires all other common files
+local linux_config = require('linux.init')
+local macOS_config = require('macOS.init')
 
 -- common configs
 common_config.apply_to_config(config)
 
 -- os-specific configs
-if _.is_linux() then
-    local linux_config = require('linux.init')
-    linux_config.apply_to_config(config)
-elseif _.is_macOS() then
-    local macOS_config = require('macOS.init')
-    macOS_config.apply_to_config(config)
+if _.is_linux() then linux_config.apply_to_config(config)
+elseif _.is_macOS() then macOS_config.apply_to_config(config)
 -- TODO:
 -- elseif _.is_windows() then
 else
