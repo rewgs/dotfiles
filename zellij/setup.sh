dc1ae7326e6095e8f8d0dd0f380d5e4f022b5874
rewgs <alexrugermusic@gmail.com>
2024-06-19 18:22:13 -0700

    hotfix

--- zellij/setup.sh
+++ zellij/setup.sh
@@ -6,12 +6,16 @@ setup::main() {
     local SRC="$(realpath $(dirname $BASH_SOURCE))/dots"
     local DST="$HOME/.config/zellij"
 
-    if [[ ! -d "$(dirname $DST)"]]; then mkdir "$(dirname $DST)"; fi
+    if [[ ! -d "$(dirname $DST)" ]]; then 
+        mkdir "$(dirname $DST)"
+    fi
+
     if [[ -d "$DST" ]] || [[ -L "$DST" ]]; then
         sudo rm -rf "$DST"
     fi
+
     if [[ ! -L "$DST" ]]; then
-        ln -s "$DST" "$DST"
+        ln -s "$SRC" "$DST"
     fi
 }
 setup::main
