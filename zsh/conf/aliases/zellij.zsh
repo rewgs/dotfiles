82cf4252a57255195c51d2124b6f348a1c3e866d
Alex Ruger <alexrugermusic@gmail.com>
2024-06-19 19:35:11 -0700

    added more

--- zsh/conf/aliases/zellij.zsh
+++ zsh/conf/aliases/zellij.zsh
@@ -1,2 +1,6 @@
 alias z="zellij"
 alias zls="zellij list-sessions"
+# alias zk="zellij kill-session $1"       # only affects active sessions
+# alias zka="zellij kill-all-sessions"
+# alias zdl="zellij delete-session $1"     # affects both active and inactive sessions
+# alias zdla="zellij delete-all-sessions"
