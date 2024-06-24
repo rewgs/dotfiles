77d3aed77dcf36b6d6b5742a263c5e67c65920b8
Alex Ruger <alexrugermusic@gmail.com>
2024-06-24 12:39:06 -0700

    Split into individual machine configs

--- nvim/dots/lazy/rewgs-thinkpad-x1/lua/rewgs/plugins/colorscheme.lua
+++ nvim/dots/lazy/rewgs-thinkpad-x1/lua/rewgs/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 local gruvbox = require("rewgs.plugins.colorschemes.gruvbox")
 local onedarkpro = require("rewgs.plugins.colorschemes.onedarkpro")
+local onedarkpro_oled = require("rewgs.plugins.colorschemes.onedarkpro_oled")
 
 return {
     -- comment out all but one
     -- gruvbox
-    onedarkpro
+    -- onedarkpro
+    onedarkpro_oled
 }
