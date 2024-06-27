771bd656bca64a2a6b57434ccad76d89c3cdc039
rewgs <alexrugermusic@gmail.com>
2024-06-27 16:19:47 -0700

    Added support for installing oh-my-posh and symlinking dotfiles on Windows

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -20,9 +20,18 @@ function Install-Oh-My-Posh {
         brew update
         brew install jandedobbeleer/oh-my-posh/oh-my-posh
     }
-#     if ($IsWindows) {
-#         Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
-#     }
+
+    if ($IsWindows) {
+        # choco
+        choco install oh-my-posh
+
+        # manual
+        # Set-ExecutionPolicy Bypass -Scope Process -Force
+        # Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+
+        # manually adds oh-my-posh to PATH
+        # $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
+    }
     else {
         Write-Output "Invalid operating system! Exiting now."
         exit
@@ -57,7 +66,7 @@ function Symlink-Profile([string]$thisFile) {
 
 
 function Main([string]$thisFile) {
-    # Install-Oh-My-Posh
+    Install-Oh-My-Posh
     Symlink-Profile $thisFile
 }
 
