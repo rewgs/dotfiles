b316590e74a7efaae8e8e2d205b5780456f05675
Alex Ruger <alexrugermusic@gmail.com>
2024-08-12 17:42:07 -0700

    Slightly increased font size for these tired old eyes

--- wezterm/dots/macOS/font.lua
+++ wezterm/dots/macOS/font.lua
@@ -3,7 +3,8 @@ local wezterm = require('wezterm')
 local module = {}
 
 function module.apply_to_config(config)
-    config.font_size = 15
+    -- config.font_size = 15
+    config.font_size = 16
     config.font = wezterm.font_with_fallback {
         { family = 'FiraCode Nerd Font', weight = 'Regular' },
 
