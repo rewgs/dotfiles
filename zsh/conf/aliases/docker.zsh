edb0f654508be9e5ff4e6f4e9dc0443d666c6c08
Alex Ruger <alexrugermusic@gmail.com>
2024-08-17 22:59:02 -0700

    Added docker alias so that sudo is always prepended if user is not root.

--- zsh/conf/aliases/docker.zsh
+++ zsh/conf/aliases/docker.zsh
@@ -1,7 +1,8 @@
 # docker
 if [[ $(uname) == "Linux" ]]; then
-    alias dcls="docker container ls"
-    alias ncst="docker start nextcloud-aio-mastercontainer"
-    alias ncsp="docker stop nextcloud-aio-mastercontainer"
-    alias rm_all_containers=remove_all_docker_containers
+    if [[ $(whoami) != "root" ]]; then
+        alias docker="sudo docker"
+    fi
+    alias dls="docker container ls"
+    alias drmac=remove_all_docker_containers
 fi
