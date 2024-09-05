375a2d1bf83deb1ef0337cb9f0dcd8a0cfabc9bd
Alex Ruger <alexrugermusic@gmail.com>
2024-09-04 22:14:34 -0700

    confined to linux only

--- zoom/setup.sh
+++ zoom/setup.sh
@@ -2,9 +2,11 @@
 #
 # Symlinks zoom `dots/zoomus.conf` file to ~/.config/zoomus.conf.
 
-dots="$(realpath $(dirname $BASH_SOURCE)/dots)"
-src="$dots/zoomus.conf"
-dst="$HOME/.config/zoomus.conf"
-
-# TODO: Check if $dst already exists.
-ln -s "$src" "$dst"
+if [[ $(uname == "Linux") ]]; then
+    dots="$(realpath $(dirname $BASH_SOURCE)/dots)"
+    src="$dots/zoomus.conf"
+    dst="$HOME/.config/zoomus.conf"
+    
+    # TODO: Check if $dst already exists.
+    ln -s "$src" "$dst"
+fi
