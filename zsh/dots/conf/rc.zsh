de2491a06243fc2bf3be585d54a7f70e4a87c877
Alex Ruger <alexrugermusic@gmail.com>
2024-09-05 15:46:48 -0700

    Reorganized and added setup script.

--- zsh/dots/conf/rc.zsh
+++ zsh/dots/conf/rc.zsh
@@ -2,15 +2,7 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # configures the `fuck` command
-if command -v thefuck &> /dev/null; then
-    eval $(thefuck --alias)
-fi
-
-# scales the display to 2x
-# this is only for Parallels on M1 Mac
-# if [[ $(uname) == "Linux" ]] && [[ $(echo $HOST) == "ubuntu-linux-22-04-desktop" ]]; then
-#     sway output "Virtual-1" scale 2
-# fi
+if command -v thefuck &> /dev/null; then eval $(thefuck --alias); fi
 
 if command -v broot &> /dev/null; then
     if [[ $(uname) == "Darwin" ]]; then
