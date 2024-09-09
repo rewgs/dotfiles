b9701d68cbc92ecaa964c54d4e5606e3049f9f9c
Alex Ruger <alexrugermusic@gmail.com>
2024-09-09 10:42:56 -0700

    Okay cool now this just isn't working for no fucking reason.

--- wezterm/dots/rewgs-thinkpad-x1/keys/tmux.lua
+++ wezterm/dots/rewgs-thinkpad-x1/keys/tmux.lua
@@ -90,6 +90,9 @@ local actions = {
             },
         },
         session = {
+            -- FIXME: Why is this not working?!?!?!? It was literally fucking 
+            -- working two seconds ago, and now it just executes a blank command.
+            -- God I fucking hate computers sometimes.
             overview = wezterm.action.Multiple {
                 wezterm.action.DisableDefaultAssignment,
                 wezterm.action.SendKey(TMUX_PREFIX),
