27b31773f41044fdfcd2d84ebef1aa595e7912a2
Alex Ruger <alexrugermusic@gmail.com>
2024-09-08 22:09:25 -0700

    Vastly simplified this file.

--- wezterm/dots/wezterm.lua
+++ wezterm/dots/wezterm.lua
@@ -1,37 +1,14 @@
 local wezterm = require('wezterm')
-local hostname = wezterm.hostname()
-print("Hostname: " .. hostname)
+
 
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/config_builder.html
 -- @type config builder object
 local config = wezterm.config_builder()
 
--- local _ = require('common.helpers') -- requires only the helper functions; this is placed in common for cleanliness
-
--- common; applies to all machines
-local common_config = require('common.init') -- requires all other common files
-common_config.apply_to_config(config)
-
--- machine-specific configs
-if hostname == 'rewgs-thinkpad-x1' then
-    local machine_config = require('rewgs-thinkpad-x1.init')
-    machine_config.apply_to_config(config)
-elseif hostname == 'rewgs-2021-macbook-air' then
-    local machine_config = require('rewgs-2021-macbook-air.init')
-    machine_config.apply_to_config(config)
-end
-
--- require(hostname .. ".init").apply_to_config(config)
-
--- common configs
-
--- if _.is_linux() then linux_config.apply_to_config(config)
--- elseif _.is_macOS() then macOS_config.apply_to_config(config)
--- TODO:
--- elseif _.is_windows() then
--- else
--- TODO: panic/throw error that OS isn't (yet) supported
--- end
+-- Requires common config, and machine-specific config (by hostname); 
+-- applies `config` to both.
+require('common.init').apply_to_config(config)
+require(wezterm.hostname() .. ".init").apply_to_config(config)
 
 
 return config
