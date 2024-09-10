2060407f5e3997da7f09c9540823b7ae38b26314
Alex Ruger <alexrugermusic@gmail.com>
2024-09-10 11:43:52 -0700

    Simplified aliases.

--- zsh/dots/conf/aliases/python.zsh
+++ zsh/dots/conf/aliases/python.zsh
@@ -1,9 +1,8 @@
-alias pipfreeze="pip freeze > requirements.txt"
+# pip
 alias pf="pip freeze > requirements.txt"
+alias pr="pip install -r requirements.txt"
 
-alias pipinstallreqs="pip install -r requirements.txt"
-alias pir="pip install -r requirements.txt"
-
+# python web server
 alias pyserve="python3 -m http.server 8080"
 
 # pyenv
@@ -12,4 +11,5 @@ alias dv="deactivate"
 alias nv=create_new_venv
 alias nvhb=create_new_venv_v3.9_with_gui_fix_for_mac
 
+# running python
 alias pm="python main.py"
