bc410bd999049f046d1bdbf24bec41532713294b
Alex Ruger <alexrugermusic@gmail.com>
2024-09-17 11:19:24 -0700

    Cleaned up, added TapeOp work dir alias.

--- zsh/dots/conf/aliases/places.zsh
+++ zsh/dots/conf/aliases/places.zsh
@@ -1,7 +1,3 @@
-# personal connections and computers
-alias nas="ssh rewgs@rewgs-nas"
-alias vm="ssh rewgs@rewgs-nas-debian-vm"
-
 places::alias() {
     local DST="$1"
     local ALIAS="$2"
@@ -10,39 +6,28 @@ places::alias() {
     fi
 }
 
+
+# personal connections and computers
+alias nas="ssh rewgs@rewgs-nas"
+alias vm="ssh rewgs@rewgs-nas-debian-vm"
+
 # personal bin
-# if [[ -d "$HOME/bin" ]]; then
-#     alias b="cd $HOME/bin"
-# fi
 places::alias "$HOME/bin" "b"
 
 # second brain
-# if [[ -d "$HOME/second-brain" ]]; then
-#     alias B="cd $HOME/second-brain"
-# fi
 places::alias "$HOME/second-brain" "B"
 
 # dotfiles
-# alias dotfiles="cd $DOTFILES"
-# alias dots="cd $DOTFILES"
-alias d="cd $DOTFILES"
-# places::alias "$DOTFILES" "d"
+places::alias "$DOTFILES" "d"
 
 # src
-# if [[ -d "$HOME/src" ]]; then
-#     alias s="cd $HOME/src"
-# fi
 places::alias "$HOME/src" "s"
 
 # work dirs
-if [[ $(uname) == "Darwin" ]]; then
-    alias naswork="cd /Volumes/main/projects"
-fi
-places::alias "$HOME/work" "w"
 
-# sns dirs
-# alias sns="cd $HOME/work/sns/"
-# alias snsd="cd $HOME/work/sns/development/"
-# alias snstodo="cd $HOME/work/sns/sns-to-do/ && nvim README.md"
+# sns
 places::alias "$HOME/work/sns" "sns"
 places::alias "$HOME/work/sns/development" "snsd"
+
+# tapeop
+places::alias "$HOME/work/TapeOp" "to"
