cff24049d48423bbd87af60e9529e53ab28452f6
Alex Ruger <alexrugermusic@gmail.com>
2024-10-03 10:45:01 -0700

    Finished setup.sh, put dotfiles into dots dir.

--- git/setup.sh
+++ git/setup.sh
@@ -1,57 +1,43 @@
 #!/bin/bash
+#
+# Symlinks `config` to `~/.gitconfig`, and `ignore` to `~/.config/git/ignore`.
 
 
-_THIS_DIR="$(realpath $(dirname "$BASH_SOURCE"))"
-# echo "$_THIS_DIR"
-
-
-# TODO: put this in libbash, source it into this file
 symlink_dotfile() {
-    # TODO: finish
-    # check_response() {
-    #     local response="$1"
-    #     local valid_response=(  "yes", "Yes", "YES", "y", 
-    #                             "no", "No", "NO", "n" )
-    #     if ! [[ "${array[@]}" =~ "$response" ]]; then
-    #         read - "${response} is not a valid response. Try again."
-    #         check_response
-    #     fi
-    # }
+    local src="$1"
+    local dst="$2"
 
-    local SRC="$1"
-    local DST="$2"
+    if [[ -L "$dst" ]] || [[ -f "$dst" ]]; then 
+        rm -f "$dst"
+    fi
 
-    if [[ -L "$DST" ]]; then rm -f "$DST"; fi
-
-    # TODO: finish when check_response() is finished. 
-    # For now, the following `if` will just forcefully delete the file, as 
-    # that's probably what I'll want to do in 90% of cases.
-    # if [[ -f "$DST" ]]; then 
-    #     read -p "$DST already exists. Do you want to overwrite it?" response
-    #     check_response "$response"
-    # fi
-    if [[ -f "$DST" ]]; then rm -f "$DST"; fi
-
-    if [[ ! -L "$DST" ]] || [[ ! -f "$DST" ]]; then
-        ln -s "$SRC" "$DST"
+    if [[ ! -L "$dst" ]] || [[ ! -f "$dst" ]]; then
+        echo "Symlinking $src to $dst"
+        ln -s "$src" "$dst"
     fi
 }
 
 symlink_config() {
+    local src="$1/config"
+    local dst="$HOME/.gitconfig"
 
-    local SRC="$(realpath "$_THIS_DIR/config")"
-    local DST="$HOME/.gitconfig/.gitconfig"
-
-    symlink_dotfile "$SRC" "$DST"
+    symlink_dotfile "$src" "$dst"
 }
 
 symlink_ignore() {
-    local SRC="$(realpath "$_THIS_DIR/ignore")"
-    local DST="$HOME/.config/git/ignore"
-    symlink_dotfile "$SRC" "$DST"
+    local src="$1/ignore"
+    local dst="$HOME/.config/git/ignore"
+    local dst_parent="$(realpath "$(dirname "$dst")")"
+    if [[ ! -d "$dst_parent" ]]; then
+        mkdir -p "$dst_parent"
+    fi
+
+    symlink_dotfile "$src" "$dst"
 }
 
 main() {
-    symlink_config
-    symlink_ignore
+    local dots="$(realpath "$(dirname "$BASH_SOURCE")/dots")"
+    # symlink_config "$dots"
+    symlink_ignore "$dots"
 }
+main
