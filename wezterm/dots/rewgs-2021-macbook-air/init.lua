5493703238e85ddf5995db029a6d3d18be4b48c9
Alex Ruger <alexrugermusic@gmail.com>
2024-10-08 22:14:03 -0700

    Changed fullscreen mode from true to false.

--- wezterm/dots/rewgs-2021-macbook-air/init.lua
+++ wezterm/dots/rewgs-2021-macbook-air/init.lua
@@ -10,7 +10,7 @@ function module.apply_to_config(config)
     -- Source: https://github.com/wez/wezterm/issues/2669
     config.window_decorations = 'RESIZE|MACOS_FORCE_DISABLE_SHADOW'
 
-    config.native_macos_fullscreen_mode = true
+    config.native_macos_fullscreen_mode = false
     config.quit_when_all_windows_are_closed = false
 
     appearance.apply_to_config(config)
