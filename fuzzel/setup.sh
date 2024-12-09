9e822b6111d80c95a878ea4783d640554eeacd2c
Alex Ruger <alexrugermusic@gmail.com>
2024-12-08 16:21:25 -0800

    Redid setup script, deleted old configs.

--- fuzzel/setup.sh
+++ fuzzel/setup.sh
@@ -2,3 +2,32 @@
 #
 # TODO:
 # Symlinks the `.ini` file with the same name as the machine's $HOSTNAME env var to `fuzzel.ini`.
+
+
+setup-fuzzel::main() {
+    local this_file="$(realpath "$(dirname "${BASH_SOURCE}")")"
+
+    local src="${this_file}/dots/${HOSTNAME}"
+    local dst="${HOME}/.config/fuzzel"
+
+    echo "$src"
+
+    if [[ ! -f "$src" ]]; then
+        echo "Machine ${HOSTNAME} does not have a fuzzel config! Exiting."
+        exit
+    fi
+
+    if [[ ! -d "$dst" ]]; then
+        mkdir -p "$dst"
+    fi
+
+
+    if [[ -f "$dst/fuzzel.ini" ]] || [[ -L "$dst/fuzzel.ini" ]]; then
+        rm -f "$dst/fuzzel.ini"
+    fi
+
+    if [[ ! -L "$dst/fuzzel.ini" ]]; then
+        ln -s "$src" "$dst/fuzzel.ini"
+    fi
+}
+setup-fuzzel::main
