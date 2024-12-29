9ac7ff745570dc1280775b59a9c67b15937a90a5
Alex Ruger <alexrugermusic@gmail.com>
2024-12-29 13:54:30 -0500

    Fixed window management and Spoons path issue.

--- hammerspoon/dots/init.lua
+++ hammerspoon/dots/init.lua
@@ -1,14 +1,14 @@
--- local home = os.getenv("HOME")
--- local src = home .. "/src"
--- Spoons = src .. "/Spoons/Spoons/"
+local spoons = os.getenv("HOME") .. "/src" .. "/Spoons" .. "/Spoons" .. "/?.spoon/init.lua"
+-- local spoons = spoonsPath .. "/?.spoon/init.lua"
+package.path = package.path .. ";" .. spoons
 
-require('modules.mod-keys')
--- require('modules.reload')
-require('modules.spaces')
-require('modules.apps.init')
+require("modules.mod-keys")
+require("modules.reload")
+-- require("modules.spaces")
+require("modules.apps.init")
 
 -- NOTE: Using Phoenix for this until I can sort out the Spoons path issue.
--- require('modules.window-management')
+require("modules.window-management")
 
 -- require('modules.window-switcher')
 -- require('modules.yabai') -- not yet ready for primetime...
--- hammerspoon/dots/modules/window-management.lua
+++ hammerspoon/dots/modules/window-management.lua
@@ -1,6 +1,6 @@
-require('modules.mod-keys')
+require("modules.mod-keys")
 
-hs.loadSpoon(Spoons .. "WindowHalfsAndThirds")
+hs.loadSpoon("WindowHalfsAndThirds")
 
 hs.window.animationDuration = 0
 
@@ -8,34 +8,34 @@ hs.window.animationDuration = 0
 -- spoon.WindowHalfsAndThirds:bindHotkeys(spoon.WindowHalfsAndThirds.defaultHotkeys)
 
 local customHotkeys = {
-    left_half       = { mod.hyper, "h" }, --
-    right_half      = { mod.hyper, "l" }, --
-    max_toggle      = { mod.hyper, "k" },
+	left_half = { mod.hyper, "h" }, --
+	right_half = { mod.hyper, "l" }, --
+	max_toggle = { mod.hyper, "k" },
 
-    center          = { mod.hyper, "c" }, -- retains size
+	center = { mod.hyper, "c" }, -- retains size
 
-    -- max             = { mod.hyper, "m" },
+	-- max             = { mod.hyper, "m" },
 
-    top_half        = { mod.oHyper, "t" },
-    bottom_half     = { mod.oHyper, "b" },
-    third_up        = { mod.soHyper, "t" },
-    third_down      = { mod.soHyper, "b" },
+	top_half = { mod.oHyper, "t" },
+	bottom_half = { mod.oHyper, "b" },
+	third_up = { mod.soHyper, "t" },
+	third_down = { mod.soHyper, "b" },
 
-    third_left      = { mod.oHyper, "h" }, -- moves *to* the left in thirds
-    third_right     = { mod.oHyper, "l" }, -- moves *to* the right in thirds
-    middle_third_h  = { mod.oHyper, "k" },
+	third_left = { mod.oHyper, "h" }, -- moves *to* the left in thirds
+	third_right = { mod.oHyper, "l" }, -- moves *to* the right in thirds
+	middle_third_h = { mod.oHyper, "k" },
 
-    left_two_third  = { mod.soHyper, "h" },
-    right_two_third = { mod.soHyper, "l" },
+	left_two_third = { mod.soHyper, "h" },
+	right_two_third = { mod.soHyper, "l" },
 
-    top_left        = { mod.hyper, "u" },
-    top_right       = { mod.hyper, "o" },
-    bottom_left     = { mod.hyper, "m" },
-    bottom_right    = { mod.hyper, "." },
+	top_left = { mod.hyper, "u" },
+	top_right = { mod.hyper, "o" },
+	bottom_left = { mod.hyper, "m" },
+	bottom_right = { mod.hyper, "." },
 
-    undo            = { mod.soHyper, "z" },
-    larger          = { mod.soHyper, "w" },  -- i.e. "wider"
-    smaller         = { mod.soHyper, "w" }, -- i.e. "opposite of wider"
+	undo = { mod.soHyper, "z" },
+	larger = { mod.soHyper, "w" }, -- i.e. "wider"
+	smaller = { mod.soHyper, "w" }, -- i.e. "opposite of wider"
 }
 
 spoon.WindowHalfsAndThirds:bindHotkeys(customHotkeys)
