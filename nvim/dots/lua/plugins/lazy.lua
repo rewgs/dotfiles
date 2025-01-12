f46ed7fbe0159b049f7b4200974d7a70ae0c19fb
Alex Ruger <alexrugermusic@gmail.com>
2025-01-12 15:39:55 -0800

    Fixed lazy.lua so that if a specific host's config does not exist, MacBook Air's config is used.

--- nvim/dots/lua/plugins/lazy.lua
+++ nvim/dots/lua/plugins/lazy.lua
@@ -1,27 +1,32 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({ "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-    {
-        { import = "plugins" .. "." .. "common" },
-        { import = "plugins" .. "." .. "common" .. "." .. "lsp" },
-        -- TODO: Handle if this directory doesn't eixst.
-        { import = "plugins" .. "." .. vim.fn.hostname() },
-    },
-    {
-        checker = {
-            enabled = true,
-            notify = false
-        },
-        change_detection = {
-            notify = false
-        }
-    }
-)
+-- If config for current host does not exist, uses MacBook Air's config.
+local hostSpecific = "plugins" .. "." .. vim.fn.hostname()
+if not hostSpecific then
+	hostSpecific = "plugins" .. "." .. "rewgs-2021-macbook-air"
+end
+
+require("lazy").setup({
+	{ import = "plugins" .. "." .. "common" },
+	{ import = "plugins" .. "." .. "common" .. "." .. "lsp" },
+	{ import = hostSpecific },
+}, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
