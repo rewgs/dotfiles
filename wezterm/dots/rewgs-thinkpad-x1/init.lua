0ed5c0d8b3fb9d1a556ac396bc9d4aa363e2e7a7
Alex Ruger <alexrugermusic@gmail.com>
2025-01-15 20:15:10 -0800

    Dunno, don't remember lol.

--- wezterm/dots/rewgs-thinkpad-x1/init.lua
+++ wezterm/dots/rewgs-thinkpad-x1/init.lua
@@ -4,8 +4,8 @@ local appearance = require('rewgs-thinkpad-x1.appearance')
 local font = require('rewgs-thinkpad-x1.font')
 
 -- local keys = require('rewgs-thinkpad-x1.keys.standard')     -- ALT|CTRL as main mod
--- local keys = require('rewgs-thinkpad-x1.keys.alt')          -- ALT key as main mod
-local keys = require('rewgs-thinkpad-x1.keys.tmux')         -- no wezterm tabs/panes, only using tmux
+local keys = require('rewgs-thinkpad-x1.keys.alt') -- ALT key as main mod
+-- local keys = require('rewgs-thinkpad-x1.keys.tmux')         -- no wezterm tabs/panes, only using tmux
 -- local keys = require('rewgs-thinkpad-x1.keys.zellij_keys')  -- no wezterm tabs/panes, only using zellij
 
 function module.apply_to_config(config)
@@ -16,4 +16,5 @@ function module.apply_to_config(config)
     font.apply_to_config(config)
     keys.apply_to_config(config)
 end
+
 return module
