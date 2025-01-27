f3210675c4d764239e1d3a1f393f0b25ea077f59
Alex Ruger <alexrugermusic@gmail.com>
2025-01-26 22:03:31 -0800

    Wrote setup script.

--- starship/setup.sh
+++ starship/setup.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 #
 # Symlinks `starship.toml` to ~/.config/starship.toml
+
+setup::main() {
+    local src
+    local name
+    local dst_dir
+    local dst
+
+    src="$(realpath "$(dirname "${BASH_SOURCE}")"/dots/starship.toml)"
+    name="$(basename "$src")"
+
+    dst_dir="${HOME}/.config"
+    dst="${dst_dir}/${name}"
+
+    if [[ ! -d "$dst_dir" ]]; then 
+        mkdir -p "$dst_dir"
+    fi
+
+    if [[ -f "$dst" ]] || [[ -L "$dst" ]]; then 
+        rm -f "$dst"
+    fi
+
+    if [[ ! -L "$dst" ]]; then 
+        ln -s "$src" "$dst"
+    fi
+}
+setup::main
