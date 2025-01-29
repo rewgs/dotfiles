56a5ebe35bdd77f4c28c30cddcad571ca68e6b5b
Alex Ruger <alexrugermusic@gmail.com>
2025-01-29 14:26:24 -0600

    Minor updates.

--- wezterm/dots/rewgs-2021-macbook-air/init.lua
+++ wezterm/dots/rewgs-2021-macbook-air/init.lua
@@ -1,23 +1,23 @@
 local module = {}
 
-local appearance = require('rewgs-2021-macbook-air.appearance')
-local font = require('rewgs-2021-macbook-air.font')
+local appearance = require("rewgs-2021-macbook-air.appearance")
+local font = require("rewgs-2021-macbook-air.font")
 
-local keys = require('rewgs-2021-macbook-air.keys.standard')
+local keys = require("rewgs-2021-macbook-air.keys.standard")
 -- local keys = require('rewgs-2021-macbook-air.keys.tmux')
 
 function module.apply_to_config(config)
-    -- The latter vastly helps overall system graphics performance.
-    -- Source: https://github.com/wez/wezterm/issues/2669
-    config.window_decorations = 'RESIZE|MACOS_FORCE_DISABLE_SHADOW'
+	-- The latter vastly helps overall system graphics performance.
+	-- Source: https://github.com/wez/wezterm/issues/2669
+	config.window_decorations = "RESIZE|MACOS_FORCE_DISABLE_SHADOW"
 
-    config.native_macos_fullscreen_mode = false
-    config.quit_when_all_windows_are_closed = false
+	config.native_macos_fullscreen_mode = true
+	config.quit_when_all_windows_are_closed = false
 
-    appearance.apply_to_config(config)
-    font.apply_to_config(config)
+	appearance.apply_to_config(config)
+	font.apply_to_config(config)
 
-    keys.apply_to_config(config)
+	keys.apply_to_config(config)
 end
 
 return module
