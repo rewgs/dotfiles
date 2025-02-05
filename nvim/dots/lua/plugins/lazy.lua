4b2eebd948ad05f465df85cba7eb73f4f894c42f
Alex Ruger <alexrugermusic@gmail.com>
2025-02-05 17:02:49 -0600

    Updated comments regarding machine-specific configs.

--- nvim/dots/lua/plugins/lazy.lua
+++ nvim/dots/lua/plugins/lazy.lua
@@ -12,8 +12,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local hostSpecific = "plugins" .. "." .. vim.fn.hostname()
+
 -- If config for current host does not exist, uses MacBook Air's config.
--- Note that 0 in this case is returned if the directory does *not* exist.
+-- NOTE: the 0 in this case is returned if the directory does *not* exist, as 0 is a false-y value in Lua.
 if vim.fn.isdirectory(hostSpecific) == 0 then
 	hostSpecific = "plugins" .. "." .. "rewgs-2021-macbook-air"
 end
