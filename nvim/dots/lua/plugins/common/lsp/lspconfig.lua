10854dec961f4fbd0ae4b7cceb0776448b02283d
Alex Ruger <alexrugermusic@gmail.com>
2025-02-06 14:09:35 -0600

    Turned off basedpyright features as well.

--- nvim/dots/lua/plugins/common/lsp/lspconfig.lua
+++ nvim/dots/lua/plugins/common/lsp/lspconfig.lua
@@ -134,6 +134,23 @@ return {
 				})
 			end,
 
+			["basedpyright"] = function()
+				lspconfig["basedpyright"].setup({
+					settings = {
+						basedpyright = {
+							-- Using Ruff's import organizer
+							disableOrganizeImports = true,
+						},
+						python = {
+							analysis = {
+								-- Ignore all files for analysis to exclusively use Ruff for linting
+								ignore = { "*" },
+							},
+						},
+					},
+				})
+			end,
+
 			["ruff"] = function()
 				lspconfig["ruff"].setup({
 					capabilities = capabilities,
