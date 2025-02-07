adaa0cc69c84c3f9f317ee45bd4d32f35e946b90
Alex Ruger <alexrugermusic@gmail.com>
2025-02-07 11:27:46 -0800

    Re-organized into dots dir; wrote setup.sh script.

---
+++ bash/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Symlinks the files in `dots` to `~`.
+
+setup-bash::main() {
+    dots="$(dirname "$(realpath "${BASH_SOURCE}")")/dots"
+    for file in "${dots}"/*; do
+        src="$(realpath "${file}")"
+        name="$(basename "${src}")"
+        dst="${HOME}/.${name}"
+
+        if [[ -f "${dst}" ]] || [[ -L "${dst}" ]]; then
+            rm "${dst}"
+        fi
+
+        if [[ ! -L "${dst}" ]]; then
+            ln -s "${src}" "${dst}"
+            echo "Symlinked:"
+            echo -e "\t${src}"
+            echo -e "\tto"
+            echo -e "\t${dst}\n"
+        fi
+    done
+}
+setup-bash::main
