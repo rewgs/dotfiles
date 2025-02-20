937f1ca0ccd80f7b681812a22a970f6dbb99b880
Alex Ruger <alexrugermusic@gmail.com>
2025-02-19 19:20:44 -0600

    Wrote and tested.

--- clang-format/setup.sh
+++ clang-format/setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 #
 # Symlinks `.clang-format` to `~/.clang-format`.
+
+function setup-clang-format::main() {
+    local src="$(dirname "$(realpath "${BASH_SOURCE}")")/.clang-format"
+    local dst="${HOME}/.clang-format"
+
+    if [[ -f "$dst" ]] || [[ -L "$dst" ]]; then
+        rm -f "$dst"
+    fi
+
+    if [[ ! -L "$dst" ]]; then
+        ln -s "$src" "$dst"
+    fi
+}
+
+setup-clang-format::main
