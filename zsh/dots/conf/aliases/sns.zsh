1f65df2d4d58e0503e5150947acab5260311db48
Alex Ruger <alexrugermusic@gmail.com>
2025-03-07 16:40:17 -0600

    Detach command.

--- zsh/dots/conf/aliases/sns.zsh
+++ zsh/dots/conf/aliases/sns.zsh
@@ -8,10 +8,14 @@ elif [[ -d "$HOME/sns" ]]; then
 elif [[ -d "$HOME/.sns" ]]; then
     SNS_DIR="$HOME/.sns"
     alias sns="cd $SNS_DIR"
+else
+    echo "SNS_DIR not set."
 fi
 
 # snsd
 if [[ -d "$SNS_DIR/development" ]]; then
     SNS_DEV="$SNS_DIR/development"
     alias snsd="cd $SNS_DEV"
+else
+    echo "SNS_DEV not set."
 fi
