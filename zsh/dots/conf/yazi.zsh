d0530386130a212308cceeb62b179a0bc83f70e5
Alex Ruger <alexrugermusic@gmail.com>
2025-03-07 16:40:27 -0600

    Added this back after merge error.

---
+++ zsh/dots/conf/yazi.zsh
@@ -0,0 +1,10 @@
+# Suggested by yazi devs:
+# This `y` shell wrapper provides the ability to change the current working directory when exiting Yazi.
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
