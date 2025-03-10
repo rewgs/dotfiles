b3c3bbf5c95ce3a7f0ab7f23f5ad29072de7ee07
Alex Ruger <alexrugermusic@gmail.com>
2025-03-09 22:15:29 -0500

    Cleaned up.

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,68 +1,24 @@
 # Sets up PowerShell Core on Linux, macOS, or Windows.
 
 
-function Install-Oh-My-Posh {
-    if ($IsLinux) {
-        # TODO: check if homebrew is installed first
-        # install homebrew
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+$thisDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$profileFile = "Microsoft.PowerShell_profile.ps1"
+$src = Join-Path -Path $thisDir -ChildPath $profileFile
 
-        brew update
-        brew install jandedobbeleer/oh-my-posh/oh-my-posh
-    }
-
-    # TODO: make these `else if` once $IsLinux branch is written; `else` branch
-    if ($IsMacOS) {
-        # TODO: check if homebrew is installed first
-        # install homebrew
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-        brew update
-        brew install jandedobbeleer/oh-my-posh/oh-my-posh
-    }
-#     if ($IsWindows) {
-#         Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
-#     }
-    else {
-        Write-Output "Invalid operating system! Exiting now."
-        exit
-    }
+if ($IsWindows) {
+    $dstDir = "~/Documents/PowerShell"
+} 
+else {
+    $dstDir = "~/.config/powershell"
 }
 
-
-function Symlink-Profile([string]$thisFile) {
-    $thisDir = Split-Path -Parent $thisFile
-    $profileFile = "Microsoft.PowerShell_profile.ps1"
-    $src = Join-Path -Path $thisDir -ChildPath $profileFile
-    
-    if ($IsWindows) {
-        $dstDir = "~/Documents/PowerShell"
-    } 
-    else {
-        $dstDir = "~/.config/powershell"
-    }
-    
-    # Creates $dstDir if it or a part of it doesn't exist (a la `mkdir -p`)
-    if (-Not (Test-Path $dstDir)) {
-        New-Item -Type Directory $dstDir
-    }
-    
-    $dst = Join-Path -Path $dstDir -ChildPath $profileFile
-    if (Test-Path $dst) {
-        Remove-Item -Path $dst
-    }
-
-    New-Item -Path $dst -ItemType SymbolicLink -Value $src
+if (-Not (Test-Path $dstDir)) {
+    New-Item -Type Directory $dstDir
 }
 
-
-function Main([string]$thisFile) {
-    # Install-Oh-My-Posh
-    Symlink-Profile $thisFile
+$dst = Join-Path -Path $dstDir -ChildPath $profileFile
+if (Test-Path $dst) {
+    Remove-Item -Path $dst
 }
 
-
-$private:_thisFile = $MyInvocation.MyCommand.Path
-# Write-Output $_thisFile
-# Write-Output $_thisFile.GetType() # string
-Main $_thisFile
+New-Item -Path $dst -ItemType SymbolicLink -Value $src
