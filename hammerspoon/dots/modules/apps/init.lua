3170099174d2807ce81436e1fea0fc3ce38cc084
Alex Ruger <alexrugermusic@gmail.com>
2025-03-10 11:55:47 -0500

    Updated bindings.

--- hammerspoon/dots/modules/apps/init.lua
+++ hammerspoon/dots/modules/apps/init.lua
@@ -5,14 +5,19 @@
 require("modules.mod-keys")
 local HYPER = Mod.hyper
 
-local TERMINAL = "ghostty"
-local BROWSER = "Safari"
+local BROWSER = "/Applications/Zen Browser.app/Contents/MacOS/zen"
+
+-- local TERMINAL = "/Applications/Ghostty.app/Contents/MacOS/ghostty"
+local TERMINAL = "/Applications/WezTerm.app/Contents/MacOS/wezterm"
+
+local MAIL = "/Applications/Mimestream.app/Contents/MacOS/Mimestream"
 
 local apps = {
 	{ mod = HYPER, key = "return", app = TERMINAL },
 	{ mod = HYPER, key = "e", app = "Finder" },
 	{ mod = HYPER, key = "i", app = BROWSER },
 	{ mod = HYPER, key = "o", app = "Obsidian" },
+	{ mod = HYPER, key = "m", app = MAIL },
 }
 
 local function bind_keys(kbs)
