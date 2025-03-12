b30b2bb6fc897d8f6ae9d5a48196e9df0ae2a68b
Alex Ruger <alexrugermusic@gmail.com>
2025-03-12 10:46:55 -0500

    Re-organized, re-wrote setup script.

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,24 +1,41 @@
 # Sets up PowerShell Core on Linux, macOS, or Windows.
 
-
-$thisDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$profileFile = "Microsoft.PowerShell_profile.ps1"
-$src = Join-Path -Path $thisDir -ChildPath $profileFile
+$_thisFile = $MyInvocation.MyCommand.Path
+$_thisDir = Split-Path -Parent $_thisFile
+$src = Join-Path -Path $_thisDir -ChildPath "dots"
 
 if ($IsWindows) {
-    $dstDir = "~/Documents/PowerShell"
-} 
-else {
-    $dstDir = "~/.config/powershell"
+    $dst = Join-Path -Path $env:HOME -ChildPath "Documents" -AdditionalChildPath "PowerShell"
+} else {
+    $dst = "$HOME/.config/powershell"
 }
 
-if (-Not (Test-Path $dstDir)) {
-    New-Item -Type Directory $dstDir
-}
-
-$dst = Join-Path -Path $dstDir -ChildPath $profileFile
 if (Test-Path $dst) {
     Remove-Item -Path $dst
 }
 
-New-Item -Path $dst -ItemType SymbolicLink -Value $src
+if (-not (Test-Path $dst)) {
+    New-Item -Path $dst -ItemType SymbolicLink -Value $src
+}
+
+# $thisDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+# $profileFile = "Microsoft.PowerShell_profile.ps1"
+# $src = Join-Path -Path $thisDir -ChildPath $profileFile
+#
+# if ($IsWindows) {
+#     $dstDir = "~/Documents/PowerShell"
+# } 
+# else {
+#     $dstDir = "~/.config/powershell"
+# }
+#
+# if (-Not (Test-Path $dstDir)) {
+#     New-Item -Type Directory $dstDir
+# }
+#
+# $dst = Join-Path -Path $dstDir -ChildPath $profileFile
+# if (Test-Path $dst) {
+#     Remove-Item -Path $dst
+# }
+#
+# New-Item -Path $dst -ItemType SymbolicLink -Value $src
