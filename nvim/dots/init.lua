10f344feb3ba31a17d0b7b0b5f36ce1577e4c4d4
rewgs <alexrugermusic@gmail.com>
2025-03-20 13:36:01 -0700

    pc rollup

--- nvim/dots/init.lua
+++ nvim/dots/init.lua
@@ -4,5 +4,17 @@
 -- require(core)
 -- require(lazy)
 
+-- Force use of zig compiler on Windows
+-- local uname = vim.loop.os_uname()
+-- if uname.sysname:find 'Windows' and uname.release:find 'Microsoft' then
+--     require ('nvim-treesitter.install').compilers = { 'zig' }
+-- end
+
+
 require("core")
 require("plugins.lazy")
+
+-- Uses clang and zig on Windows
+if vim.loop.os_uname().version:match("Windows") then
+    require('nvim-treesitter.install').compilers = { 'clang', 'zig' }
+end
--- nvim/dots/lua/plugins/common/telescope.lua
+++ nvim/dots/lua/plugins/common/telescope.lua
@@ -1,49 +1,49 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"folke/todo-comments.nvim",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+        "folke/todo-comments.nvim",
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
 
-		telescope.setup({
-			defaults = {
-				layout_strategy = "horizontal",
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
-						width = { padding = 0 },
-						height = { padding = 0 },
-						preview_width = 0.66,
-					},
-				},
-				path_display = { "smart" }, -- intelligently truncuates file names if very long
-				mappings = {
-					i = { -- keymaps for insert mode
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					},
-				},
-			},
-		})
+        telescope.setup({
+            defaults = {
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        width = { padding = 0 },
+                        height = { padding = 0 },
+                        preview_width = 0.66,
+                    },
+                },
+                path_display = { "smart" }, -- intelligently truncuates file names if very long
+                mappings = {
+                    i = {                   -- keymaps for insert mode
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    },
+                },
+            },
+        })
 
-		telescope.load_extension("fzf")
-		-- telescope.load_extension("refactoring")
+        -- telescope.load_extension("fzf")
+        -- telescope.load_extension("refactoring")
 
-		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-		vim.keymap.set("n", "<leader> ", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" }) -- was previously <leader>fs
-		vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor" })
-		vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
-		vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Search through help" })
+        vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+        vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+        vim.keymap.set("n", "<leader> ", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" }) -- was previously <leader>fs
+        vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor" })
+        vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
+        vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Search through help" })
 
-		-- todo-comments keymaps/integration
-		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comments" })
-	end,
+        -- todo-comments keymaps/integration
+        vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comments" })
+    end,
 }
