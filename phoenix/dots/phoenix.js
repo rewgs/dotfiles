03ca8f18aba23488a605c1d7054f796184f7ff95
Alex Ruger <alexrugermusic@gmail.com>
2025-03-20 19:39:28 -0700

    Added quarter window snapping.

--- phoenix/dots/phoenix.js
+++ phoenix/dots/phoenix.js
@@ -1,9 +1,8 @@
 // mod keys
-const HYPER = [ 'command', 'control' ];
-const OHYPER = ['command', 'option', 'control'];
-const SHYPER = ['command', 'control', 'shift'];
-const SOHYPER = ['command', 'option', 'control', 'shift'];
-
+const HYPER = ["command", "control"];
+const OHYPER = ["command", "option", "control"];
+const SHYPER = ["command", "control", "shift"];
+const SOHYPER = ["command", "option", "control", "shift"];
 
 //-----------------------------------------------------------------------------
 // META
@@ -19,125 +18,158 @@ Phoenix.set({
 //     Phoenix.notify("Reloaded");
 // });
 
-
 //-----------------------------------------------------------------------------
 // WINDOW MANAGEMENT
 //-----------------------------------------------------------------------------
 
-// const focusWest = Key.on("h", OHYPER, () => { 
+// const focusWest = Key.on("h", OHYPER, () => {
 //     const window = Window.focused();
 //     window.focusClosestNeighbor("west");
 // });
 //
 // const focusSouth = Key.on("j", OHYPER, () => {
 //     const window = Window.focused();
-//     window.focusClosestNeighbor("south"); 
+//     window.focusClosestNeighbor("south");
 // });
 //
-// const focusNorth = Key.on("k", OHYPER, () => { 
+// const focusNorth = Key.on("k", OHYPER, () => {
 //     const window = Window.focused();
 //     window.focusClosestNeighbor("north");
 // });
 //
-// const focusEast = Key.on("l", OHYPER, () => { 
+// const focusEast = Key.on("l", OHYPER, () => {
 //     const window = Window.focused();
 //     window.focusClosestNeighbor("east");
 // });
-
-
-
 
 //--------------------------------------
 // window tiling
 //--------------------------------------
 
-const maximized = Key.on('k', HYPER, () => {
-    Window.focused().maximize();
+const maximized = Key.on("k", HYPER, () => {
+  Window.focused().maximize();
 });
 
 const leftHalf = Key.on("h", HYPER, () => {
-    // Phoenix.notify("Snapping left half");
-    const window = Window.focused(),
-    screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x,
-        y: screenFrame.y,
-        width: screenFrame.width / 2,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    width: screenFrame.width / 2,
+    height: screenFrame.height,
+  });
 });
 
 const rightHalf = Key.on("l", HYPER, () => {
-    // Phoenix.notify("Snapping right half");
-    const window = Window.focused();
-    const screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x + screenFrame.width / 2,
-        y: screenFrame.y,
-        width: screenFrame.width / 2,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 2,
+    y: screenFrame.y,
+    width: screenFrame.width / 2,
+    height: screenFrame.height,
+  });
 });
 
 const leftThird = Key.on("h", OHYPER, () => {
-    const window = Window.focused();
-    const screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x,
-        y: screenFrame.y,
-        width: screenFrame.width / 3,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    width: screenFrame.width / 3,
+    height: screenFrame.height,
+  });
 });
 
 const centerThird = Key.on("k", OHYPER, () => {
-    const window = Window.focused();
-    const screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x + screenFrame.width / 3,
-        y: screenFrame.y,
-        width: screenFrame.width / 3,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 3,
+    y: screenFrame.y,
+    width: screenFrame.width / 3,
+    height: screenFrame.height,
+  });
 });
 
 const rightThird = Key.on("l", OHYPER, () => {
-    const window = Window.focused();
-    const screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x + screenFrame.width / 1.5,
-        y: screenFrame.y,
-        width: screenFrame.width / 3,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 1.5,
+    y: screenFrame.y,
+    width: screenFrame.width / 3,
+    height: screenFrame.height,
+  });
 });
 
 const leftTwoThirds = Key.on("h", SOHYPER, () => {
-    const window = Window.focused();
-    const screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x,
-        y: screenFrame.y,
-        width: screenFrame.width / 1.5,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    width: screenFrame.width / 1.5,
+    height: screenFrame.height,
+  });
 });
 
 const rightTwoThirds = Key.on("l", SOHYPER, () => {
-    const window = Window.focused();
-    const screenFrame = window.screen().flippedVisibleFrame();
-    window.setFrame({
-        x: screenFrame.x + screenFrame.width / 3,
-        y: screenFrame.y,
-        width: screenFrame.width / 1.5,
-        height: screenFrame.height,
-    });
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 3,
+    y: screenFrame.y,
+    width: screenFrame.width / 1.5,
+    height: screenFrame.height,
+  });
 });
 
-// TODO: bottom left quarter
-// TODO: bottom right quarter
-// TODO: top left quarter
-// TODO: top right quarter
+const upperLeftQuarter = Key.on(",", HYPER, () => {
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x,
+    y: screenFrame.y,
+    width: screenFrame.width / 2,
+    height: screenFrame.height / 2,
+  });
+});
+
+const upperRightQuarter = Key.on(".", HYPER, () => {
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 2,
+    y: screenFrame.y,
+    width: screenFrame.width / 2,
+    height: screenFrame.height / 2,
+  });
+});
+
+const lowerLeftQuarter = Key.on(",", SHYPER, () => {
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x,
+    y: screenFrame.y + screenFrame.height / 2,
+    width: screenFrame.width / 2,
+    height: screenFrame.height / 2,
+  });
+});
+
+const lowerRightQuarter = Key.on(".", SHYPER, () => {
+  const window = Window.focused();
+  const screenFrame = window.screen().flippedVisibleFrame();
+  window.setFrame({
+    x: screenFrame.x + screenFrame.width / 2,
+    y: screenFrame.y + screenFrame.height / 2,
+    width: screenFrame.width / 2,
+    height: screenFrame.height / 2,
+  });
+});
 
 // Key.on("[", SOHYPER, () => {
 //     debug("Center and resize to 2/3 of screen");
@@ -151,24 +183,22 @@ const rightTwoThirds = Key.on("l", SOHYPER, () => {
 //     });
 // });
 
-
-
 //--------------------------------------
 // spaces
 //--------------------------------------
 
-const moveWindowToNextSpace = Key.on('=', HYPER, () => {
-    const space = Space.active();
-    const window = Window.focused();
-    space.next().moveWindows([window]);
-    window.focus();
+const moveWindowToNextSpace = Key.on("=", HYPER, () => {
+  const space = Space.active();
+  const window = Window.focused();
+  space.next().moveWindows([window]);
+  window.focus();
 });
 
-const moveWindowToPrevSpace = Key.on('-', HYPER, () => {
-    const space = Space.active();
-    const window = Window.focused();
-    space.next().moveWindows([window]);
-    window.focus();
+const moveWindowToPrevSpace = Key.on("-", HYPER, () => {
+  const space = Space.active();
+  const window = Window.focused();
+  space.next().moveWindows([window]);
+  window.focus();
 });
 
 // const missionControl = Key.on("up", HYPER, () => {
