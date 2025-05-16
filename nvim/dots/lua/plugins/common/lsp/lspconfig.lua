b9ab524cc19b161cd1e8ab2464c22f40d01bf38f
Alex Ruger <alexrugermusic@gmail.com>
2025-05-15 22:34:12 -0700

    Temporarily redoing lsp setup. Copied this lspconfig file https://github.com/mezdelex/neovimconfig.

--- nvim/dots/lua/plugins/common/lsp/lspconfig.lua
+++ nvim/dots/lua/plugins/common/lsp/lspconfig.lua
@@ -1,224 +1,43 @@
 return {
 	"neovim/nvim-lspconfig",
-	opts = {
-		inlay_hints = { enabled = true },
-	},
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"mason-org/mason-lspconfig.nvim",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
-	},
 	config = function()
-		local lspconfig = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-		-- What to do when an LSP client attaches to a buffer
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = {
-					buffer = ev.buf,
-					silent = true,
-					-- vim.lsp.inlay_hint.enable(),
-				}
-
-				-- set keybindings
-				-- opts.desc = "Toggle inlay hints"
-				-- vim.keymap.set("n", "<leader>gh", vim.lsp.inlay_hint.enable, opts)
-
-				opts.desc = "Show LSP references"
-				vim.keymap.set("n", "<leader>gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-				opts.desc = "Go to declaration"
-				vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-				opts.desc = "Show LSP definitions"
-				vim.keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-				opts.desc = "Show LSP implementations"
-				vim.keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-				opts.desc = "Show LSP type definitions"
-				vim.keymap.set("n", "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-				opts.desc = "See available code actions"
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Smart rename"
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-				opts.desc = "Show buffer diagnostics"
-				vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-				opts.desc = "Show line diagnostics"
-				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-				opts.desc = "Go to previous diagnostic"
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-				opts.desc = "Go to next diagnostic"
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-				opts.desc = "Show documentation for what is under cursor"
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-				opts.desc = "Restart LSP"
-				vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-			end,
+		require("mason").setup({
+			registries = { "github:crashdummyy/mason-registry", "github:mason-org/mason-registry" },
 		})
-		-- vim.lsp.inlay_hint.enable(bufnr, true)
+		require("mason-lspconfig").setup()
+		require("roslyn").setup()
 
-		-- Used to enable autocompletion (assigned to every LSP server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
-		-- mason_lspconfig.setup_handlers({
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"clangd",
-				"gopls",
-				"lua_ls",
+		require("blink.cmp").setup({
+			completion = {
+				documentation = { auto_show = true },
 			},
+			keymap = { preset = "enter" },
+		})
 
-			-- Default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-
-			["gopls"] = function()
-				lspconfig["gopls"].setup({
-					capabilities = capabilities,
-					settings = {
-						gopls = {
-							analyses = { shadow = true, unusedwrite = true, unusedvariable = true },
-							staticcheck = true,
-							gofumpt = true,
-							hints = {
-								assignVariableTypes = true,
-								compositeLiteralFields = true,
-								compositeLiteralTypes = true,
-								constantValues = true,
-								functionTypeParameters = true,
-								parameterNames = true,
-								rangeVariableTypes = true,
-							},
-						},
-					},
-				})
-
-				-- lspconfig["gopls"].setup({
-				-- 	settings = {
-				-- 		gopls = {
-				-- 			analyses = {
-				-- 				unusedparams = true,
-				-- 			},
-				-- 			staticcheck = true,
-				-- 			gofumpt = true,
-				-- 			hints = {
-				-- 				assignVariableTypes = true,
-				-- 				compositeLiteralFields = true,
-				-- 				compositeLiteralTypes = true,
-				-- 				constantValues = true,
-				-- 				functionTypeParameters = true,
-				-- 				parameterNames = true,
-				-- 				rangeVariableTypes = true,
-				-- 			},
-				-- 		},
-				-- 	},
-				-- })
-			end,
-
-			-- Custom handlers for specified servers
-			-- Lua
-			["lua_ls"] = function()
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- Makes the language server recognize "vim" global
-							diagnostics = {
-								disable = { "missing-fields" },
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
-
-			-- Using pylsp as LSP, but turning off all plugins/etc and letting ruff handle all of that.
-			-- ["pylsp"] = function()
-			-- 	lspconfig["pylsp"].setup({
-			-- 		settings = {
-			-- 			pylsp = {
-			-- 				plugins = {
-			-- 					pyflakes = { enabled = false },
-			-- 					pycodestyle = { enabled = false },
-			-- 					autopep8 = { enabled = false },
-			-- 					yapf = { enabled = false },
-			-- 					mccabe = { enabled = false },
-			-- 					pylsp_mypy = { enabled = false },
-			-- 					pylsp_black = { enabled = false },
-			-- 					pylsp_isort = { enabled = false },
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	})
-			-- end,
-
-			-- ["pyright"] = function()
-			-- 	lspconfig["pyright"].setup({
-			-- 		settings = {
-			-- 			pyright = {
-			-- 				-- Using Ruff's import organizer
-			-- 				disableOrganizeImports = true,
-			-- 			},
-			-- 			python = {
-			-- 				analysis = {
-			-- 					-- Ignore all files for analysis to exclusively use Ruff for linting
-			-- 					ignore = { "*" },
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	})
-			-- end,
-
-			-- ["basedpyright"] = function()
-			-- 	lspconfig["basedpyright"].setup({
-			-- 		settings = {
-			-- 			basedpyright = {
-			-- 				-- Using Ruff's import organizer
-			-- 				disableOrganizeImports = true,
-			-- 			},
-			-- 			python = {
-			-- 				analysis = {
-			-- 					-- Ignore all files for analysis to exclusively use Ruff for linting
-			-- 					ignore = { "*" },
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	})
-			-- end,
-
-			-- ["ruff"] = function()
-			-- 	lspconfig["ruff"].setup({
-			-- 		capabilities = capabilities,
-			-- 		settings = {},
-			-- 	})
-			-- end,
+		vim.diagnostic.config({
+			signs = {
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+				},
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.WARN] = "",
+				},
+			},
+			update_in_insert = true,
+			virtual_text = true,
 		})
 	end,
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"seblyng/roslyn.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"williamboman/mason.nvim",
+		{ "saghen/blink.cmp", build = "cargo build --release" },
+	},
 }
