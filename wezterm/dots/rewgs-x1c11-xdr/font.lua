54244e10b41c6521a3a5bce2566a9d2473dd0579
Alex Ruger <alexrugermusic@gmail.com>
2025-05-24 17:32:27 -0700

    Renamed configs with ' kanshi-profile' at end of name.

--- wezterm/dots/rewgs-x1c11-xdr/font.lua
+++ wezterm/dots/rewgs-x1c11-xdr/font.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-	config.font_size = 11
+	config.font_size = 13
 	config.font = wezterm.font({
 		family = "FiraCode NerdFont Retina",
 		weight = "Regular",
---
+++ wezterm/dots/rewgs-x1c11-xdr/init.lua
@@ -0,0 +1,18 @@
+local module = {}
+
+local host = "rewgs-x1c11"
+
+local appearance = require(host .. ".appearance")
+local font = require(host .. ".font")
+local keys = require(host .. ".keys.alt") -- ALT key as main mod
+
+function module.apply_to_config(config)
+	config.enable_wayland = true
+	config.window_decorations = "NONE"
+
+	appearance.apply_to_config(config)
+	font.apply_to_config(config)
+	keys.apply_to_config(config)
+end
+
+return module
