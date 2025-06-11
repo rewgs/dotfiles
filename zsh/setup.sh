0fa777e9d26d0b33e300b816b34d8e0f6e731cb7
Alex Ruger <alexrugermusic@gmail.com>
2025-06-10 23:09:37 -0700

    Fixed linking issue.

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -14,14 +14,14 @@ function clone-plugin() {
     local repo="$1"
     local name="${repo##*/}" # Trims the last part of the URL, i.e. the name of the repo
     local src="$HOME/src"
-    local dst="$zsh_plugins_dir"
+    local dst="$zsh_plugins_dir/$name"
 
     if [[ ! -d "$src" ]] || [[ ! -L "$src" ]]; then
         mkdir -p "$src"
     fi
 
     if [[ ! -d "$dst" ]] || [[ ! -L "$dst" ]]; then
-        git clone "$repo" "$dst/$name"
+        git clone "$repo" "$dst"
     fi
 }
 
@@ -32,38 +32,49 @@ function clone-plugins() {
     clone-plugin "https://github.com/zsh-users/zsh-syntax-highlighting"
 }
 
+function get-dots-path() {
+    local dots
+
+    if ! command -v realpath &>/dev/null; then
+        dots="$(readlink -f "$(dirname "$BASH_SOURCE")"/dots)"
+    else
+        dots="$(realpath "$(dirname "$BASH_SOURCE")"/dots)"
+    fi
+
+    echo "$dots"
+}
+
 # Symlinks: $DOTFILES/zsh/dots/.zshenv -> ~/.zshenv
 function setup-zshenv() {
-    local dots
     local src
     local dst
 
-    src="$dots/.zshenv"
+    src="$(get-dots-path)/.zshenv"
     dst="$HOME/.zshenv"
-
-    if command -v realpath &>/dev/null; then
-        dots="$(realpath "$(dirname "$BASH_SOURCE")"/dots)"
-    elif command -v readlink &>/dev/null; then
-        dots="$(readlink -f "$(dirname "$BASH_SOURCE")"/dots)"
-    else
-        exit 1
-    fi
 
     if [[ -f "$dst" ]] || [[ -L "$dst" ]]; then
         rm -f "$dst"
     fi
 
     if [[ ! -f "$dst" ]] || [[ ! -L "$dst" ]]; then
+        echo -e "\nLinking:"
+        echo -e "\t$src"
+        echo -e "\tto"
+        echo -e "\t$dst\n"
         ln -s "$src" "$dst"
     fi
 }
 
 # Symlinks: $DOTFILES/zsh/dots/conf -> $XDG_CONFIG_DIR/zsh (i.e. $ZDOTDIR)
 function setup-zsh-conf() {
-    local dots="$(realpath "$(dirname "$BASH_SOURCE")"/dots)"
-    local src="$dots/conf"
-    local dst="$HOME/.config/zsh"
-    local config="$(dirname "$dst")"
+    local src
+    local dst
+    local config
+
+    src="$(get-dots-path)/conf"
+    dst="$HOME/.config/zsh"
+
+    config="$(dirname "$dst")"
 
     if [[ ! -d "$config" ]]; then
         mkdir "$config"
@@ -74,6 +85,10 @@ function setup-zsh-conf() {
     fi
 
     if [[ ! -d "$dst" ]] || [[ ! -L "$dst" ]]; then
+        echo -e "\nLinking:"
+        echo -e "\t$src"
+        echo -e "\tto"
+        echo -e "\t$dst\n"
         ln -s "$src" "$dst"
     fi
 }
