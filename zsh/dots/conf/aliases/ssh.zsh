54f774be193850613e65cd5ce5c9b92fb06373cd
Alex Ruger <alexrugermusic@gmail.com>
2025-08-21 23:06:47 -0700

    cleaned up functions, changed some aliases.

--- zsh/dots/conf/aliases/ssh.zsh
+++ zsh/dots/conf/aliases/ssh.zsh
@@ -1,16 +1,11 @@
-alias dlssh=download_file_from_server_as_name
-
-# alias lnx="ssh rewgs@rewgs-arch"
-alias pi="ssh pi@192.168.1.72"
-
-
-#==============================================================================
-# NAS
-#==============================================================================
+# nas
 alias nas="ssh rewgs@rewgs-nas"
 
-# Arch VM
-alias dev="ssh rewgs@rewgs-nas-arch-vm"
+# arch desktop
+alias dev="ssh rewgs@rewgs-arch"
 
-# Debian VM
+# arch VM
+alias devm="ssh rewgs@rewgs-nas-arch-vm"
+
+# debian VM
 alias deb="ssh rewgs@rewgs-nas-debian-vm"
