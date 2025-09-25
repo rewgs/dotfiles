e1a40cda42b3de82cf4fe1e422d87e276773ad6b
Alex Ruger <alexrugermusic@gmail.com>
2025-09-24 22:08:16 -0700

    Tried to change font size, but wezterm appears to be broken.

--- wezterm/dots/rewgs-x1c11 laptop/font.lua
+++ wezterm/dots/rewgs-x1c11 laptop/font.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-	config.font_size = 11
+	config.font_size = 18
 	config.font = wezterm.font({
 		family = "FiraCode NerdFont",
 		weight = "Regular",
