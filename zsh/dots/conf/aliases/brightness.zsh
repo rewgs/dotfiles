7294f9376409429e028d543ee3aaf593cd42966d
Alex Ruger <alexrugermusic@gmail.com>
2025-11-24 11:05:27 -0800

    Deleted since these just will not work correctly.

--- zsh/dots/conf/aliases/brightness.zsh
+++
@@ -1,6 +0,0 @@
-# Brightness for Pro Display XDR va asdcontrol
-if [[ "$(uname)" == "Linux" ]]; then
-    alias br="sudo asdcontrol /dev/usb/hiddev7"
-    alias bu="sudo asdcontrol /dev/usb/hiddev7 -- +$1"
-    alias bd="sudo asdcontrol /dev/usb/hiddev7 -- -$1"
-fi
