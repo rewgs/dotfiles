84fd962d2040ab2a912b5d1a402930a98f0c7d0c
Alex Ruger <alexrugermusic@gmail.com>
2026-01-02 11:51:10 -0500

    Worked on go_to_dotfiles function.

--- zsh/dots/conf/aliases/places.zsh
+++ zsh/dots/conf/aliases/places.zsh
@@ -6,6 +6,30 @@ places::alias() {
     fi
 }
 
+function go_to_dotfiles() {
+    local num_args="$#"
+
+    if [[ "$num_args" -eq 0 ]]; then
+        if [[ -n "${DOTFILES+x}" ]]; then
+            cd "$DOTFILES"
+            return
+        fi
+
+        echo "environment variable DOTFILES not set."
+        return
+    fi
+
+    local dst="$DOTFILES"
+    for arg in "$@"; do
+        dst="${dst}/${arg}"
+    done
+
+    # TODO: Check if $dst exists before cd
+
+    cd "$dst"
+    return
+}
+
 # personal bin
 places::alias "$HOME/bin" "b"
 
