2b3e2f54f48d09988eef01d44b3bedb4b20e80cb
Alex Ruger <alexrugermusic@gmail.com>
2026-01-02 11:39:50 -0500

    More work on Ruby setup script.

--- hypr/setup.rb
+++ hypr/setup.rb
@@ -17,13 +17,37 @@ require 'socket'
 #   end
 # end
 
-def main
-  directories = Dir.glob('./dots/*/')
-  directories.each do |dir|
-    if dir.basename == Socket.gethostname
-      # testing
-      puts dir
+##
+# @returns [Boolean, String|nil] whether a matching directory was found, and the path if so.
+def find_matching_dir(dir)
+  Dir.each_child(dir) do |child|
+    # puts "Checking child: #{child}"
+    if child == Socket.gethostname
+      # puts "Found matching directory for hostname: #{child}"
+      return true, File.join(dir, child)
     end
+  end
+  # puts "No matching directory found for hostname: #{Socket.gethostname}"
+  return false, nil
+end
+
+def main
+  this_file = File.expand_path(File.dirname(__FILE__))
+  # puts "This file is located at: #{this_file}"
+
+  dots_dir = File.join(this_file, 'dots')
+  # puts "Dots directory is located at: #{dots_dir}"
+
+  found, dir = find_matching_dir(dots_dir)
+  if found
+    puts "Found matching dotfiles directory for this machine: #{dir}"
+  else
+    puts "No matching dotfiles directory found for this machine (hostname: #{Socket.gethostname})."
+    puts "Please choose one of the following options:"
+    puts "1) Use a pre-existing dotfiles directory."
+    puts "2) Copy a pre-existing dotfiles directory for this machine."
+
+    print "Enter your choice (1 or 2): "
   end
 end
 
