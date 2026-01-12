f844c72b6585ade6950ab057a447e130be55cc81
Alex Ruger <alexrugermusic@gmail.com>
2026-01-11 23:17:53 -0500

    Wrote setup script.

--- weechat/setup.sh
+++ weechat/setup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 #
-# Symlinks dots dir to ~/.config/weechat .
+# Symlinks dots dir to ~/.config/weechat
+
+function main() {
+    local src="$(realpath "$(dirname "${BASH_SOURCE}")"/dots)"
+    local dst="$HOME/.config/weechat"
+
+    if [[ -d "$dst" ]]; then
+        rm -rf "$dst"
+    fi
+
+    if [[ ! -L "$dst" ]]; then
+        ln -s "$src" "$dst"
+    fi
+
+    return 0
+}
+main
