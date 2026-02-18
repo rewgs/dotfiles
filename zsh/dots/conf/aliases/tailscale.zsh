33a57ae54ee6df90dcc92723cf8d1aba0c1f566d
Alex Ruger <alexrugermusic@gmail.com>
2026-02-17 17:34:16 -0800

    Commented out everything; see comments.

--- zsh/dots/conf/aliases/tailscale.zsh
+++ zsh/dots/conf/aliases/tailscale.zsh
@@ -1,8 +1,11 @@
 # tailscale
-if [[ $(uname) == "Darwin" ]] && \
-[[ ! -f "$HOME/go/bin/tailscale" ]] && \
-[[ -f "/Applications/Tailscale.app/Contents/MacOS/Tailscale" ]]; then
-    alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
-fi
 
-alias tshosts=tailscale_get_magic_dns_hostnames
+# NOTE: Removed because the golden path is to symlink this to /usr/local/bin/tailscale
+# if [[ $(uname) == "Darwin" ]] && \
+# [[ ! -f "$HOME/go/bin/tailscale" ]] && \
+# [[ -f "/Applications/Tailscale.app/Contents/MacOS/Tailscale" ]]; then
+#     alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+# fi
+
+# NOTE: Removed because I never use this.
+# alias tshosts=tailscale_get_magic_dns_hostnames
