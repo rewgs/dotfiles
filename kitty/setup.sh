fd76ff6cae7ddabb3bab4985dd123859dc9c0bd4
Alex Ruger <alexrugermusic@gmail.com>
2026-02-27 20:03:40 -0800

    x2100 rollup

--- kitty/setup.sh
+++ kitty/setup.sh
@@ -2,6 +2,7 @@
 #
 # Symlinks the `dots` dir to `~/.config/kitty`.
 # NOTE: This will silently delete `~/.config/kitty` (whether it is a directory or symlink)!
+# TODO: This needs to be updated to pick the directory that matches the machine's hostname, not just the dots dir.
 
 function main {
     local src="$(realpath "$(dirname "${BASH_SOURCE}")/dots")"
