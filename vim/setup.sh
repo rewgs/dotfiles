3fae4a23b8d84b5ae26dc9ebdfc58f4e10b09a94
Alex Ruger <alexrugermusic@gmail.com>
2026-03-11 13:03:18 -0700

    Edited setup script like all others in this branch (see comments at top of file for details). Also moved vimrc into dots dir for consistency.

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
+#
+# Symlinks dots/vimrc to ~/.vimrc.
+# Run with -n/--dry-run flag to show what will happen and not actually execute.
 
-
-setup () {
-    # gets the absolute file of this script's parent dir
-    local SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-    # echo "$SCRIPTPATH"
-
-    local src="$SCRIPTPATH/vimrc"
-    local dst="$HOME/.vimrc"
-
-    ln -s "$src" "$dst"
+run() {
+    if $DRY_RUN; then
+        echo "[dry-run] $*"
+    else
+        "$@"
+    fi
 }
-setup
+
+main() {
+    local src dst
+    src="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/dots/vimrc"
+    dst="${HOME}/.vimrc"
+
+    if [[ -f "${dst}" ]] || [[ -L "${dst}" ]]; then
+        run rm "${dst}"
+    fi
+
+    run ln -s "${src}" "${dst}"
+
+    if ! $DRY_RUN; then
+        echo "Symlinked:"
+        echo -e "\t${src}"
+        echo -e "\tto"
+        echo -e "\t${dst}"
+    fi
+}
+
+DRY_RUN=false
+
+while [[ "$1" == -* ]]; do
+    case "$1" in
+    -n | --dry-run) DRY_RUN=true ;;
+    esac
+    shift
+done
+
+$DRY_RUN && echo "NOTE: This is a dry-run. No changes will be made."
+
+main
