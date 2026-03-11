dfddfc11a803015048c5b9f92b916330ad83252a
Alex Ruger <alexrugermusic@gmail.com>
2026-03-11 11:17:11 -0700

    Added option for dry-run.

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 #
-# Symlinks the files in `dots` to `~`.
+# Symlinks the files in dots dir to $HOME.
 
-setup-bash::main() {
-    dots="$(dirname "$(realpath "${BASH_SOURCE}")")/dots"
-    for file in "${dots}"/*; do
+run() {
+    if $DRY_RUN; then
+        echo "[dry-run] $*"
+    else
+        "$@"
+    fi
+}
+
+main() {
+    for file in "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/dots"/*; do
+        local src
+        local dst
+
         src="$(realpath "${file}")"
-        name="$(basename "${src}")"
-        dst="${HOME}/.${name}"
+        dst="${HOME}/.$(basename "${src}")"
 
         if [[ -f "${dst}" ]] || [[ -L "${dst}" ]]; then
-            rm "${dst}"
+            run rm "${dst}"
         fi
 
-        if [[ ! -L "${dst}" ]]; then
-            ln -s "${src}" "${dst}"
-            echo "Symlinked:"
-            echo -e "\t${src}"
-            echo -e "\tto"
-            echo -e "\t${dst}\n"
+        if $DRY_RUN || [[ ! -L "${dst}" ]]; then
+            run ln -s "${src}" "${dst}"
+            if ! $DRY_RUN; then
+                echo "Symlinked:"
+                echo -e "\t${src}"
+                echo -e "\tto"
+                echo -e "\t${dst}\n"
+            fi
         fi
     done
 }
-setup-bash::main
+
+DRY_RUN=false
+
+while [[ "$1" == -* ]]; do
+    case "$1" in
+    -n | --dry-run) DRY_RUN=true ;;
+    esac
+    shift
+done
+
+$DRY_RUN && echo "NOTE: This is a dry-run. No changes will be made."
+
+main
