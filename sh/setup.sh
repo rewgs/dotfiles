a0b506e1fb5609feb02282b1f6a6e2ef532340b3
Alex Ruger <alexrugermusic@gmail.com>
2026-03-11 12:26:46 -0700

    Updated setup script to be similar to bash's.

--- sh/setup.sh
+++ sh/setup.sh
@@ -1,25 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 #
-# Symlinks the files in `dots` to `~`.
+# Symlinks the files in dots dir to $HOME.
+# Run with -n/--dry-run flag to show what will happen and not actually execute.
 
-setup-sh::main() {
-    dots="$(dirname "$(realpath "${BASH_SOURCE}")")/dots"
-    for file in "${dots}"/*; do
-        src="$(realpath "${file}")"
-        name="$(basename "${src}")"
-        dst="${HOME}/.${name}"
+run() {
+    if [ "$DRY_RUN" = "true" ]; then
+        echo "[dry-run] $*"
+    else
+        "$@"
+    fi
+}
 
-        if [[ -f "${dst}" ]] || [[ -L "${dst}" ]]; then
-            rm "${dst}"
+main() {
+    dots_dir="$(dirname "$(realpath "$0")")/dots"
+
+    for file in "$dots_dir"/*; do
+        src="$(realpath "$file")"
+        dst="$HOME/.$(basename "$src")"
+
+        if [ -f "$dst" ] || [ -L "$dst" ]; then
+            run rm "$dst"
         fi
 
-        if [[ ! -L "${dst}" ]]; then
-            ln -s "${src}" "${dst}"
-            echo "Symlinked:"
-            echo -e "\t${src}"
-            echo -e "\tto"
-            echo -e "\t${dst}\n"
+        if [ "$DRY_RUN" = "true" ] || [ ! -L "$dst" ]; then
+            run ln -s "$src" "$dst"
+            if [ "$DRY_RUN" != "true" ]; then
+                printf "Symlinked:\n\t%s\n\tto\n\t%s\n\n" "$src" "$dst"
+            fi
         fi
     done
 }
-setup-sh::main
+
+DRY_RUN=false
+
+while [ "${1#-}" != "$1" ]; do
+    case "$1" in
+        -n | --dry-run) DRY_RUN=true ;;
+    esac
+    shift
+done
+
+[ "$DRY_RUN" = "true" ] && echo "NOTE: This is a dry-run. No changes will be made."
+
+main
