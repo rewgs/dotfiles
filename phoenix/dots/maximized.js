fe9e9b51b22f67686664f0bb6a60cfeddd20e92a
rewgs <alexrugermusic@gmail.com>
2026-05-20 12:04:46 -0700

    Tweaks

--- phoenix/dots/maximized.js
+++
@@ -1,9 +0,0 @@
-import { hyper } from "./mods";
-
-const maximize = Key.on("k", hyper, () => {
-  Window.focused().maximize();
-});
-
-export default {
-    maximize
-}
--- phoenix/dots/mods.js
+++
@@ -1,4 +0,0 @@
-const hyper = ["command", "control"];
-const ohyper = ["command", "option", "control"];
-const shyper = ["command", "control", "shift"];
-const sohyper = ["command", "option", "control", "shift"];
